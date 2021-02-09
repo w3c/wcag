1560f9fc6127d5f6aba4dabf1d811e1bc6b3f035
Michael Cooper <cooper@w3.org>
2021-02-09 12:26:59 -0500

    put project page in repo

---
+++ project.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<?php include "_head.phi"; ?>
+		<title>Accessibility Guidelines Working Group Project Plan</title>
+	</head>
+	<body>
+		<?php include "_header.phi"; ?>
+		<h1>Accessibility Guidelines Working Group (AG WG) Project Plan</h1>
+		
+		<p>Content on this page has moved to the <a href="https://www.w3.org/WAI/GL/wiki/Timelines/">AG WG Timelines wiki page</a>.
+		
+		<!--
+		
+		<p>This page contains the project plan for the proposed Accessibility Guidelines Working Group (AGWG). The plan includes information about specifications and milestones indicated in the AGWG Charter. The dates below are tentative, used for internal planning purposes, and may change. </p>
+		
+		<p>The intent of the Accessibility Guidelines Working Group is to publish updated guidance for accessibility frequently enough to reflect technology evolution but slowly enough to allow meaningful uptake of new guidance. The Working Group completed WCAG 2.1 in June 2018, and is planning WCAG 2.2 for November 2020. In parallel the group is working on a major restructuring of accessibility guidelines, code-named "Silver", whose first release is currently targeted for November 2022.</p>
+		
+		<h2>Milestones</h2>
+		<p>The <a href="charter">Working Group charter</a> proposed initial milestones. Updates to those milestones will be provided here as needed.</p>
+		
+		<p>Last update: September 2020</p>
+		
+		<table>
+			<caption>Milestones </caption>
+			<thead>
+				<tr>
+					<th rowspan="2" scope="col">Specification</th>
+					<th rowspan="2" scope="col">Status (<?php echo date("j F Y", getlastmod()); ?>)</th>
+					<th colspan="5" scope="colgroup">Projection</th>
+				</tr>
+				<tr>
+					<th scope="col"><abbr title="First Working Draft">FPWD</abbr></th>
+					<th scope="col"><abbr title="Candidate Recommendation">CR</abbr></th>
+					<th scope="col"><abbr title="Proposed Recommendation">PR</abbr></th>
+					<th scope="col"><abbr title="Recommendation">Rec</abbr></th>
+					<th scope="col">Note</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<th colspan="7">Recommendation track deliverables</th>
+				</tr>
+				<tr>
+					<th><a href="https://w3c.github.io/wcag/guidelines/22/">WCAG 2.2</a></th>
+					<td>Under development in the Working Group.</td>
+					<td>February 2020</td>
+					<td>February 2021</td>
+					<td>April 2021</td>
+					<td>June 2021</td>
+					<td/>
+				</tr>
+				<tr>
+					<th><a href="https://w3c.github.io/silver/guidelines/">Silver (provisional name)</a></th>
+					<td>Under development primarily in the Silver Community Group and Silver Task Force with increasing coordination with the Working Group.</td>
+					<td>November 2020</td>
+					<td>November 2022</td>
+					<td>August 2023</td>
+					<td>November 2023</td>
+					<td/>
+				</tr>
+			</tbody>
+			<tbody>
+				<tr>
+					<th colspan="7">Key informative resources</th>
+				</tr>
+				<tr>
+					<th><a href="https://w3c.github.io/wcag/requirements/22/">Requirements for WCAG 2.2</a></th>
+					<td>Under development in the Working Group, not intended to publish to TR.</td>
+					<td/>
+					<td/>
+					<td/>
+					<td/>
+					<td/>
+				</tr>
+				<tr>
+					<th><a href="https://w3c.github.io/silver/requirements/">Requirements for Silver (provisional name)</a></th>
+					<td>Being maintained as a Community Group draft report, not intended to publish to TR.</td>
+					<td/>
+					<td/>
+					<td/>
+					<td/>
+					<td/>
+				</tr>
+				<tr>
+					<th><a href="https://www.w3.org/WAI/WCAG21/Understanding/">Understanding WCAG 2.1</a></th>
+					<td>Content updated weekly.</td>
+					<td/>
+					<td/>
+					<td/>
+					<td/>
+					<td/>
+				</tr>
+				<tr>
+					<th><a href="https://www.w3.org/WAI/WCAG21/Techniques/">WCAG 2.1 Techniques</a></th>
+					<td>Content updated weekly.</td>
+					<td/>
+					<td/>
+					<td/>
+					<td/>
+					<td/>
+				</tr>
+			</tbody>
+		</table>
+		<p>Note: Understanding WCAG 2.0 and Techniques for WCAG 2.0 are no longer maintained on the Technical Reports Page. Understanding WCAG 2.1 and WCAG 2.1 Techniques replaces these.</p>
+		
+		-->
+
+		<?php include "_footer.phi"; ?>
+	</body>
+</html>
