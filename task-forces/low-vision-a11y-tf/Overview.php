d23db5a69b2327783bf7403d3ea9860d8a0eadb2
Michael Cooper <cooper@w3.org>
2021-04-07 10:19:01 -0400

    LVTF pages

---
+++ task-forces/low-vision-a11y-tf/Overview.php
@@ -0,0 +1,160 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-US" lang="en-US">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link href="http://www.w3.org/WAI/wai-main.css" rel="stylesheet" type="text/css" />
+<!--[if IE 7]><link rel="stylesheet" href="/WAI/ie-old.css" type="text/css" media="all" /><![endif]-->
+<title>Low Vision Accessibility Task Force</title>
+<!-- Copyright 2000, 2001, 2002, 2003 Macromedia, Inc. All rights reserved. -->
+</head>
+<body>
+<div id="controls">
+  <ul>
+    <li><a href="#skip"><img src="/Icons/downinpage.png" alt="" /> Skip to Content </a></li>
+    <li><a href="/WAI/changedesign.html"> | Change text size or colors </a> </li>
+  </ul>
+</div>
+<div id="masthead">
+  <p id="logos"><a href="http://www.w3.org/" title="W3C Home"><img alt="W3C logo" src="/Icons/w3c_home" /></a><a href="http://www.w3.org/WAI/" title="WAI Home"><img alt="Web Accessibility initiative" src="/WAI/images/wai-temp" /></a></p>
+</div>
+<div id="tagline">
+  <p>WAI: Strategies, guidelines, resources to make the Web accessible to people with disabilities</p>
+</div>
+<div id="nav">
+  <h2>Site Navigation</h2>
+  <p id="w3cnav"><a href="http://www.w3.org/">W3C Home</a></p>
+  <p id="subhomenav"><a href="/WAI/">Web Accessibility Initiative (WAI) Home</a></p>
+  <ol id="nav1">
+    <li><a href="http://www.w3.org/WAI/GL/">AG Working Group</a>
+      <ol type="1">
+        <li id="current-node" class="current-nav "><span class="label"><span id="current-icon">»</span><a href="./">Low Vision Accessibility Task Force</a></span>
+          <ol type="i">
+            <li class="external"><a href="wiki/">LVTF Wiki <img src="/Icons/tr.png" alt="different format" /></a></li>
+            <li class="external"><a href="minutes">Minutes <img src="/Icons/tr.png" alt="different format" /></a></li>
+            <li class="external"><a href="http://www.w3.org/WAI/GL/low-vision-a11y-tf/work-statement.php">Work Statement</a> </li>
+            <li class="external"><a href="https://www.w3.org/2000/09/dbwg/details?group=81151&amp;public=1">Current Participants <img src="/Icons/tr.png" alt="different format" /></a></li>
+            <li class="external"><a href="https://lists.w3.org/Archives/Public/public-low-vision-a11y-tf/">E-mail Archives <img src="/Icons/tr.png" alt="different format" /></a></li>
+          </ol>
+        </li>
+      </ol>
+    </li>
+  </ol>
+</div>
+<div id="main">
+  <div id="skipwrapper"> <a id="skip">-</a></div>
+  <h1>Low Vision Accessibility Task Force (LVTF) </h1>
+  <p> The Low Vision Accessibility Task Force (LVTF) was created as a joint task force of the <a href="http://www.w3.org/WAI/GL/">Accessibility Guidelines Working Group (AG WG)</a> and the <a href="http://www.w3.org/WAI/UA/">User Agent Accessibility Guidelines Working Group (UAWG)</a>. The UAWG has closed. The objective of the Task Force is to address web accessibility issues   specific to people with low vision.  </p><div id="contents">
+    <h2>Page Contents</h2>
+    <ul>
+      <li><a href="#status">Announcements</a></li>
+      <li><a href="#objective">Publications</a></li>
+      <li><a href="#approach">Task Force Meetings</a></li>
+      <li><a href="#about">About</a></li>
+      <li><a href="#participation">Participation </a></li>
+      <li><a href="#patentpolicy">Patent Policy</a></li>
+    </ul>
+  </div>
+  <div>
+    <h2 id="status">Announcements </h2>
+    <ul>
+      <li>The LVTF work is listed in the <a href="https://www.w3.org/WAI/GL/low-vision-a11y-tf/wiki/Main_Page">LVTF main wiki page</a>.</li>
+    </ul>
+  </div>
+  <div>
+    <h2 id="publications">Publications</h2>
+    <p><a href="https://www.w3.org/WAI/GL/low-vision-a11y-tf/wiki/LVTF_Documents">LVTF Documents</a> lists the documents the LVTF is working on and plans to work on.</p>
+  </div>
+  <div>
+    <h2 id="m335intw">Task Force Meetings</h2>
+    <h3>Teleconference</h3>
+    <p>The Task Force meets weekly by teleconference, and plans to hold  face-to-face meetings 1-2 times a year. </p>
+    <ul>
+      <li><strong>Teleconference</strong>: Wednesdays at 10:30am, Eastern US time (GMT
+        -4 14:30). Check <a href="http://www.timeanddate.com/worldclock/converter.html?hour=9&amp;min=30&amp;sec=0&amp;p1=43">world clock</a> for other timezones.. </li>
+      <li><strong>IRC channel</strong>: <a href="irc://irc.w3.org/lvtf">#lvtf</a></li>
+      <li><strong>WebEx Meeting Number:</strong><span id="mc-txt-number">646 045 716</span><br />
+        <strong>Meeting Link</strong>: <a href="https://mit.webex.com/mit/j.php?MTID=m2486867b0c5117ca58dffc7aee66ca29" target="_blank">https://mit.webex.com/mit/j.php?MTID=m2486867b0c5117ca58dffc7aee66ca29</a> <br />
+        <strong>Mobile Auto Dial</strong>:+1-617-324-0000,,,<span id="mc-txt-number2">646 045 716</span># </li>
+    </ul>
+    <h3>Asynchronous Participation</h3>
+    <ul>
+      <li><strong>Email</strong>: <a href="mailto:public-low-vision-a11y-tf@w3.org">public-low-vision-a11y-tf@w3.org</a> (public <a href="https://lists.w3.org/Archives/Public/public-low-vision-a11y-tf/">archive</a>)</li>
+      <li><strong>Wiki</strong>: <a href="https://www.w3.org/WAI/GL/low-vision-a11y-tf/wiki/Main_Page">https://www.w3.org/WAI/GL/low-vision-a11y-tf/wiki/Main_Page</a></li>
+      <li><strong>Surveys</strong>: <a href="https://www.w3.org/2002/09/wbs/81151/">https://www.w3.org/2002/09/wbs/81151/</a></li>
+    </ul>
+    <h2 id="about">About the Low Vision Accessibility Task Force</h2>
+    <p>The Low Vision Accessibility Task Force (LVTF) was created as a task force of the <a href="http://www.w3.org/WAI/GL/">Accessibility Guidelines Working Group (AG WG)</a> and, before it closure, the <a href="http://www.w3.org/WAI/UA/">User Agent Accessibility Guidelines Working Group (UAWG)</a>. The UAWG has closed.</p>
+    <p>The objective of the Task Force is to address web accessibility issues   specific to people with low vision.  Task Force work  includes:</p>
+    <ul>
+      <li>Document accessibility requirements/needs for people with low vision</li>
+      <li>Identify gaps between user needs and existing requirements in <a href="https://www.w3.org/WAI/intro/wcag">WCAG</a>, <a href="https://www.w3.org/WAI/intro/uaag">UAAG</a>, <a href="https://www.w3.org/WAI/intro/atag">ATAG</a></li>
+      <li>Document sufficient techniques, advisory techniques, and failures for WCAG 2.0 related to low vision</li>
+      <li>Develop an extension to WCAG 2.0 to address low vision</li>
+      <li>Refine <a href="http://www.w3.org/TR/UAAG20-Reference/">user agent use cases</a> for low vision, as needed</li>
+      <li>Develop support materials to explain the issues and implementation of the above resources effectively</li>
+    </ul>
+    <h3>Facilitators and Contacts</h3>
+    <ul>
+      <li>Jim Allan, Facilitator</li>
+      <li>Andrew Kirkpatrick, Facilitator</li>
+      <li>Shawn Lawton Henry, W3C Staff Contact</li>
+    </ul>
+    <h3>Work Statement</h3>
+    <ul>
+      <li><a href="work-statement">Low Vision Accessibility Task Force Work Statement</a></li>
+    </ul>
+    <h3>Mailing List</h3>
+    <p><a href="mailto:public-low-vision-a11y-tf@w3.org">public-low-vision-a11y-tf@w3.org</a> (public <a href="https://lists.w3.org/Archives/Public/public-low-vision-a11y-tf/">archive</a>)</p>
+    <p>When using the <acronym title="Low vision accessibility task force">Low Vision Accessibility Task Force </acronym> mailing list:</p>
+    <ol>
+      <li>Treat other list members with respect.</li>
+      <li>Keep comments within the scope of this group (see <a href="work-statement">Low Vision Accessibility Task Force Work Statement</a>).</li>
+      <li>When raising an issue:
+        <ul>
+          <li>provide a clear subject line including the topic it relates          to;</li>
+          <li>explain the issue briefly;</li>
+          <li>propose a brief &amp; specific solution;</li>
+          <li>for longer comments, put a summary at the beginning.</li>
+        </ul>
+      </li>
+      <li>When replying:
+        <ul>
+          <li>keep your reply comments near the top of a message;</li>
+          <li>only quote essential parts of a previous message;</li>
+          <li>if there are multiple topics, break them into separate emails to          facilitate threading and to keep the subject line accurate.</li>
+        </ul>
+      </li>
+      <li>Please send any questions about the mailing list itself to the      Facilitators or Staff Contacts, not the whole list.</li>
+      <li>No soliciting.</li>
+    </ol>
+    <h2 id="participation">Participation</h2>
+    <p>To join the Low Vision Accessibility Task Forces, individuals must be participants of the AG WG. Participants are expected to actively contribute to the work of LVTF, including:</p>
+    <ul>
+      <li>Minimum 4 hours per week of LVTF work (this time  counts towards the individual's participation requirement in the AG WG);</li>
+      <li>Complete <a href="https://www.w3.org/2002/09/wbs/81151/all">LVTF surveys</a>;</li>
+      <li>Remain current on the <a href="https://lists.w3.org/Archives/Public/public-low-vision-a11y-tf/">LVTF mailing list</a> and respond in a timely manner to postings;</li>
+      <li>Participate in LVTF teleconferences, or record regrets in the <a rel="nofollow" href="https://www.w3.org/2002/09/wbs/81151/LVTF-telecons/">Availability for Upcoming LVTF Teleconferences Survey</a>.</li>
+    </ul>
+    <p>To become a participant of Low Vision Accessibility Task Force or with questions, contact <a href="mailto:akirkpat@adobe.com,jimallan@tsbvi.edu,shawn@w3.org?subject=Low Vision Accessibility Task Force Enquiry">Andrew Kirkpatrick, Jim Allan, and Shawn Henry</a></p>
+    <p><a href="https://www.w3.org/2000/09/dbwg/details?group=81151&amp;public=1">Current Low Vision A11Y TF participants </a></p>
+    <h3 id="patents">Patent Disclosures </h3>
+    <p>This Task Force is part of the <a href="http://www.w3.org/WAI/GL/charter">AG WG Charter</a> and <a href="http://www.w3.org/WAI/UA/2010/uawg_charter">UAWG Charter</a>, which operates under the <a href="http://www.w3.org/Consortium/Patent-Policy-20040205/">W3C Patent Policy</a> (5 February 2004 Version). W3C maintains a public list of any patent disclosures made in connection with the deliverables of the <a href="http://www.w3.org/2004/01/pp-impl/35422/status" rel="disclosure">AG WG</a> and the <a href="http://www.w3.org/2004/01/pp-impl/36791/status">UAWG</a>.</p>
+    <!--       <h2 id="additional"><a name="Additional" id="Additional" shape="rect"></a>Additional Information</h2> -->
+    <h2 id="timeline">&nbsp;</h2>
+  </div>
+</div>
+<div id="footer">
+  <h2>Document Information</h2>
+  <p class="status"><strong>Status: </strong>Updated $Date: 2017/01/27 20:22:12 $</p>
+  <div class="footer-nav">
+    <p>[<a href="http://www.w3.org/WAI/sitemap.html">WAI Site Map</a>] [<a href="http://www.w3.org/WAI/sitehelp.html">Help with WAI Website</a>] [<a href="http://www.w3.org/WAI/search.php">Search</a>] [<a href="/WAI/contacts">Contacting WAI</a>]<br clear="none" />
+      <strong>Feedback welcome to <a href="mailto:public-low-vision-a11y-tf@w3.org">public-low-vision-a11y-tf@w3.org</a></strong> (a publicly archived list) or <a href="mailto:wai@w3.org">wai@w3.org</a> (a WAI staff-only list).</p>
+  </div>
+  <div class="copyright">
+    <p><a rel="Copyright" href="/Consortium/Legal/ipr-notice#Copyright">Copyright</a> © 2016 <a href="/"><acronym title="World Wide Web Consortium">W3C</acronym></a><sup>®</sup> (<a href="http://www.csail.mit.edu/"><acronym title="Massachusetts Institute of Technology">MIT</acronym></a>, <a href="http://www.ercim.org/"><acronym title="European Research Consortium for Informatics and Mathematics">ERCIM</acronym></a>, <a href="http://www.keio.ac.jp/">Keio</a>, <a href="http://ev.buaa.edu.cn/">Beihang</a>), All Rights Reserved. W3C <a href="/Consortium/Legal/ipr-notice#Legal_Disclaimer">liability</a>, <a href="/Consortium/Legal/ipr-notice#W3C_Trademarks">trademark</a>, <a rel="Copyright" href="/Consortium/Legal/copyright-documents">document use</a> and <a rel="Copyright" href="/Consortium/Legal/copyright-software">software licensing</a> rules apply. Your interactions with this site are in accordance with our <a href="/Consortium/Legal/privacy-statement#Public">public</a> and <a
+						href="/Consortium/Legal/privacy-statement#Members">Member</a> privacy statements.</p>
+  </div>
+  <!-- end footer --> 
+</div>
+</body>
+</html>
---
+++ task-forces/low-vision-a11y-tf/work-statement.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-US" lang="en-US">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link href="http://www.w3.org/WAI/wai-main.css" rel="stylesheet" type="text/css" />
+<!--[if IE 7]><link rel="stylesheet" href="/WAI/ie-old.css" type="text/css" media="all" /><![endif]-->
+<title>Low Vision Accessibility Task Force Work Statement</title>
+</head>
+<body>
+<div id="controls">
+   <ul>
+      <li><a href="#skip"><img src="/Icons/downinpage.png" alt="" /> Skip to Content </a></li>
+      <li><a href="/WAI/changedesign.html"> | Change text size or colors </a> </li>
+   </ul>
+</div>
+<div id="masthead">
+   <p id="logos"><a href="http://www.w3.org/" title="W3C Home"><img alt="W3C logo" src="/Icons/w3c_home" /></a><a href="http://www.w3.org/WAI/" title="WAI Home"><img alt="Web Accessibility initiative" src="/WAI/images/wai-temp" /></a></p>
+</div>
+<div id="tagline">
+   <p>WAI: Strategies, guidelines, resources to make the Web accessible to people with disabilities</p>
+</div>
+<div id="nav">
+   <h2>Site Navigation</h2>
+   <p id="w3cnav"><a href="http://www.w3.org/">W3C Home</a></p>
+   <p id="subhomenav"><a href="/WAI/">Web Accessibility Initiative (WAI) Home</a></p>
+   <ol id="nav1">
+      <li><a href="http://www.w3.org/WAI/GL/">AG Working Group</a><ol type="1">
+					<li><a href="./">Low Vision Accessibility Task Force</a></span>
+							<ol type="i">
+								<li class="external"><a href="wiki/">LVTF Wiki <img src="/Icons/tr.png" alt="different format" /></a></li>
+								<li class="external"><a href="minutes">Minutes <img src="/Icons/tr.png" alt="different format" /></a></li>
+								<li id="current-node" class="current-nav "><span class="label"><span id="current-icon">»</span>Work Statement</li>
+								<li class="external"><a href="https://www.w3.org/2000/09/dbwg/details?group=81151&amp;public=1">Current Participants <img src="/Icons/tr.png" alt="different format" /></a></li>
+								<li class="external"><a href="http://lists.w3.org/Archives/Public/public-low-vision-a11y-tf/">E-mail Archives <img src="/Icons/tr.png" alt="different format" /></a></li>
+							</ol>
+						</li>
+					</ol></li>
+   </ol>
+</div>
+<div id="main">
+   <div id="skipwrapper"> <a id="skip">-</a></div>
+   <h1>Low Vision Accessibility Task Force Work Statement</h1>
+   <p class="highlight-version">This Work Statement was approved in 2015. Updated information is available from the <a href="https://www.w3.org/WAI/GL/low-vision-a11y-tf/">Low Vision Accessibility Task Force home page</a> and <a href="https://www.w3.org/WAI/GL/low-vision-a11y-tf/wiki/Main_Page">wiki page</a>.</p>
+   <p> The Low Vision Accessibility Task Force (Low Vision A11Y TF) is a task force of the <a href="http://www.w3.org/WAI/GL/">Accessibility Guidelines Working Group (AG WG)</a> (formerly also of the <a href="http://www.w3.org/WAI/UA/">User Agent Accessibility Guidelines Working Group (UAWG)</a> which has closed). It assists this Working Group with the work identified below.</p>
+   <div id="contents">
+      <h2>Page Contents</h2>
+      <ul>
+         <li><a href="#status">Status</a></li>
+         <li><a href="#objective">Objective</a></li>
+         <li><a href="#approach">Approach</a></li>
+         <li><a href="#dependencies">Dependencies</a></li>
+					<li><a href="#communication">Communication</a></li>
+         <li><a href="#participation">Participation </a></li>
+         <li><a href="#patentpolicy">Patent Policy</a></li>
+      </ul>
+   </div>
+   <div>
+      <h2 id="status">Status</h2>
+      <p>The Low Vision Accessibility Force is now active. The <a href=".">Task Force home page</a> contains information about the operation and resources of the group.</p>
+      <p>This Work Statement was updated 27 January 2017 to change the name of the sponsoring Working Group to "Accessibility Guidelines", and previously was updated to clarify that it is now a task force just of the AG WG because the UAWG closed, and to update a liasion from the PFWG, which has closed, to the APA WG.</p>
+   </div>
+   <div>
+      <h2 id="objective">Objective</h2>
+      <p>The objective of the Low Vision Accessibility Task Force is to identify issues and specify solutions to web accessibility issues specific to users with low vision via WCAG 2.0 techniques, understanding and guidance documents, and as needed to produce extensions to WCAG 2.0. Since many of the low vision issues may be addressed through the browser, this work will also include references and use cases related to UAAG 2.0. This work includes:</p>
+      <ul>
+         <li>Document requirements for low vision users;</li>
+					<li>Identify gaps in the above requirements between user needs and how they are met by web sites that conform to WCAG 2.0;</li>
+					<li>Document sufficient and advisory techniques and failures for WCAG 2.0 to meet low vision user needs;</li>
+					<li>Define low vision use cases which user agents need to support; </li>
+					<li>Develop extension to WCAG 2.0 to address low-vision user needs effectively;</li>
+         <li>Develop support materials to explain the issues and implementation of the above resources effectively.</li>
+      </ul>
+   </div>
+   <div>
+      <h2 id="approach">Approach</h2>
+      <p>Initially the Task Force will refine the scope of work need to provide complete information on how to support low vision accessibility. The work will likely be broken down into modular components that can be used as independent resources or as part of a cohesive suite. The individual components of the Low Vision A11Y Task Force's work may be developed as W3C Recommendations, W3C Working Group Notes, or other W3C / WAI resources. This will be decided after the development of an initial, more detailed set of requirements for handling accessibility for low vision users.</p>
+      <p>The work will be carried out iteratively with continual involvement of the public throughout the development. In particular, key stakeholders such as developers, evaluators, experts, researchers, and users will be regularly involved in the development process of the work of the Low Vision Accessibility Task Force.</p>
+   </div>
+   <div>
+     <h2 id="dependencies">Dependencies</h2>
+      <p>The Low Vision Accessibility Task Force depends on the <a href="http://www.w3.org/WAI/GL/">AG WG</a> to ensure accurate interpretation and representation of WCAG requirements.</p>
+      <p>The Low Vision Accessibility Task Force will also coordinate with the <a href="http://www.w3.org/WAI/APA/">Accessible Platform Architectures Working Group (APA WG)</a>, for technical advice and <a href="http://www.w3.org/WAI/EO/">Education and Outreach Working Group (EOWG)</a> where necessary to improve the writing, presentation, and educational value of the developed materials.</p>
+   </div>
+   <div>
+      <h2 id="communication">Communication</h2>
+      <p>Low Vision Accessibility Task Force communications are publicly visible. Communication mechanisms for the Low Vision Accessibility Task Force include:</p>
+      <ul>
+         <li>Low Vision Accessibility Task Force mailing list public-low-vision-a11y-tf@w3.org and <a href="http://lists.w3.org/Archives/Public/public-low-vision-a11y-tf/">mailing list archive</a> ;</li>
+					<li>Use of a <a href="wiki/">wiki</a> to share development of resources;</li>
+					<li><a href="https://www.w3.org/2002/09/wbs/81151/">Web-based surveys</a> to poll group opinion;</li>
+					<li>IRC discussion on the <a href="irc://irc.w3.org/lvtf">#lvtf</a> IRC channel, used largely for minute-taking;</li>
+         <li>Weekly teleconferences, <a href="minutes">minutes</a> from the teleconferences are also sent to AG WG mailing list;</li>
+         <li>Regular updates and feedback to the AG WG, and potentially the WAI CC as needed;</li>
+         <li>Regular submission of deliverables to the AG WG for approval;</li>
+         <li>Summary of current status and progress on the <a href="./">Low Vision Accessibility Task Force home page</a>.</li>
+      </ul>
+   </div>
+   <div>
+      <h2 id="participation">Participation</h2>
+      <p>To join the Low Vision A11Y TF, individuals must be participants of the AG WG. Participants are expected to actively contribute to the work of Low Vision A11Y TF, including:</p>
+      <ul>
+         <li>Minimum 4 hours per week of Low Vision A11Y TF work (this time also counts towards the individual's participation requirement in the sponsoring WG through which they have joined);</li>
+         <li>Remain current on the Low Vision Accessibility Task Force mailing list and respond in a timely manner to postings;</li>
+         <li>Participate in Low Vision Accessibility Task Force telephone meetings, or send regrets to the Low Vision Accessibility Task Force mailing list.</li>
+      </ul>
+      <p>Contact <a href="mailto:akirkpat@adobe.com,jimallan@tsbvi.edu?subject=Low Vision Accessibility Task Force Enquiry">Andrew Kirkpatrick, and Jim Allan</a> to become a participant of Low Vision Accessibility Task Force or with questions.</p>
+      <div>
+         <h3>Leadership</h3>
+         <p>Staff contacts oversee attention to W3C Process with respect to the chartered requirements. The Facilitator(s) set agenda, lead meetings, determine consensus, and are the primary liaison to the WGs.</p>
+					<ul>
+						<li>
+							<strong>Task Force facilitators:</strong> Jim Allan &amp; Andrew Kirkpatrick</li>
+						<li> <strong>Staff Contact: </strong><a href="https://www.w3.org/People/Shawn/">Shawn Lawton Henry</a> </li>
+					</ul>
+      </div>
+      <div>
+         <h3>Participants</h3>
+         <p><a href="https://www.w3.org/2000/09/dbwg/details?group=81151&amp;public=1">Current Low Vision A11Y TF participants </a></p>
+      </div>
+   </div>
+   <div>
+      <h2 id="patentpolicy">Patent Policy</h2>
+      <p> This Task Force is part of the <a href="http://www.w3.org/WAI/GL/charter">AG WG Charter</a>, which operates under the <a href="http://www.w3.org/Consortium/Patent-Policy-20040205/">W3C Patent Policy</a> (5 February 2004 Version). W3C maintains a public list of any patent disclosures made in connection with the <a href="http://www.w3.org/2004/01/pp-impl/35422/status" rel="disclosure">deliverables of the AG WG</a>.</p>
+   </div>
+</div>
+<div id="footer">
+   <h2>Document Information</h2>
+   <p class="status"><strong>Status: </strong>Approved in 2015.</p>
+   <div class="footer-nav">
+      <p>[<a href="http://www.w3.org/WAI/sitemap.html">WAI Site Map</a>] [<a href="http://www.w3.org/WAI/sitehelp.html">Help with WAI Website</a>] [<a href="http://www.w3.org/WAI/search.php">Search</a>] [<a href="/WAI/contacts">Contacting WAI</a>]<br clear="none" />
+					<strong>Feedback welcome to <a href="mailto:wai-wcag-editor@w3.org">wai-wcag-editor@w3.org</a></strong> (a publicly archived list) or <a href="mailto:wai@w3.org">wai@w3.org</a> (a WAI staff-only list).</p>
+   </div>
+   <div class="copyright">
+      <p><a rel="Copyright" href="/Consortium/Legal/ipr-notice#Copyright">Copyright</a> © 2015 <a href="/"><acronym title="World Wide Web Consortium">W3C</acronym></a><sup>®</sup> (<a href="http://www.csail.mit.edu/"><acronym title="Massachusetts Institute of Technology">MIT</acronym></a>, <a href="http://www.ercim.org/"><acronym title="European Research Consortium for Informatics and Mathematics">ERCIM</acronym></a>, <a href="http://www.keio.ac.jp/">Keio</a>, <a href="http://ev.buaa.edu.cn/">Beihang</a>), All Rights Reserved. W3C <a href="/Consortium/Legal/ipr-notice#Legal_Disclaimer">liability</a>, <a href="/Consortium/Legal/ipr-notice#W3C_Trademarks">trademark</a>, <a rel="Copyright" href="/Consortium/Legal/copyright-documents">document use</a> and <a rel="Copyright" href="/Consortium/Legal/copyright-software">software licensing</a> rules apply. Your interactions with this site are in accordance with our <a href="/Consortium/Legal/privacy-statement#Public">public</a> and <a
+						href="/Consortium/Legal/privacy-statement#Members">Member</a> privacy statements.</p>
+   </div>
+   <!-- end footer -->
+</div>
+</body>
+</html>
