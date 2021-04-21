22dac62e71be72b59542b4f91cfff0e391c0284d
Shawn Lawton Henry (fka slhenry) <shawn@w3.org>
2021-04-21 11:08:00 -0500

    LVTF page on WAI website - update April 2021 (#1735)
    * Update Overview.php
    
    * Update Overview.php
    
    * Fix spacing and spelling of publication.
    
    Fix spacing and spelling of publication.
    
    * Update Overview.php
    
    Co-authored-by: Jonathan Avila <jon.avila@ssbbartgroup.com>

--- task-forces/low-vision-a11y-tf/Overview.php
+++ task-forces/low-vision-a11y-tf/Overview.php
@@ -42,8 +42,9 @@
 </div>
 <div id="main">
   <div id="skipwrapper"> <a id="skip">-</a></div>
-  <h1>Low Vision Accessibility Task Force (LVTF) </h1>
-  <p> The Low Vision Accessibility Task Force (LVTF) was created as a joint task force of the <a href="http://www.w3.org/WAI/GL/">Accessibility Guidelines Working Group (AG WG)</a> and the <a href="http://www.w3.org/WAI/UA/">User Agent Accessibility Guidelines Working Group (UAWG)</a>. The UAWG has closed. The objective of the Task Force is to address web accessibility issues   specific to people with low vision.  </p><div id="contents">
+  <h1>Low Vision Accessibility Task Force (LVTF)</h1>
+  <p> The Low Vision Accessibility Task Force (LVTF) is a task force of the <a href="http://www.w3.org/WAI/GL/">Accessibility Guidelines Working Group (AG WG)</a>. The objective of the Task Force is to address web accessibility issues specific to people with low vision.</p>
+  <div id="contents">
     <h2>Page Contents</h2>
     <ul>
       <li><a href="#status">Announcements</a></li>
@@ -55,53 +56,45 @@
     </ul>
   </div>
   <div>
-    <h2 id="status">Announcements </h2>
-    <ul>
-      <li>The LVTF work is listed in the <a href="https://www.w3.org/WAI/GL/low-vision-a11y-tf/wiki/Main_Page">LVTF main wiki page</a>.</li>
-    </ul>
+    <h2 id="status">Announcements</h2>
+    <p>The LVTF work is listed in the <a href="https://www.w3.org/WAI/GL/low-vision-a11y-tf/wiki/Main_Page">LVTF main wiki page</a>.</p>
   </div>
   <div>
     <h2 id="publications">Publications</h2>
-    <p><a href="https://www.w3.org/WAI/GL/low-vision-a11y-tf/wiki/LVTF_Documents">LVTF Documents</a> lists the documents the LVTF is working on and plans to work on.</p>
+    <p><a href="https://www.w3.org/WAI/GL/low-vision-a11y-tf/wiki/LVTF_Documents">LVTF Documents wiki page</a> lists LVTF publications.</p>
   </div>
   <div>
-    <h2 id="m335intw">Task Force Meetings</h2>
-    <h3>Teleconference</h3>
-    <p>The Task Force meets weekly by teleconference, and plans to hold  face-to-face meetings 1-2 times a year. </p>
-    <ul>
-      <li><strong>Teleconference</strong>: Wednesdays at 10:30am, Eastern US time (GMT
-        -4 14:30). Check <a href="http://www.timeanddate.com/worldclock/converter.html?hour=9&amp;min=30&amp;sec=0&amp;p1=43">world clock</a> for other timezones.. </li>
-      <li><strong>IRC channel</strong>: <a href="irc://irc.w3.org/lvtf">#lvtf</a></li>
-      <li><strong>WebEx Meeting Number:</strong><span id="mc-txt-number">646 045 716</span><br />
-        <strong>Meeting Link</strong>: <a href="https://mit.webex.com/mit/j.php?MTID=m2486867b0c5117ca58dffc7aee66ca29" target="_blank">https://mit.webex.com/mit/j.php?MTID=m2486867b0c5117ca58dffc7aee66ca29</a> <br />
-        <strong>Mobile Auto Dial</strong>:+1-617-324-0000,,,<span id="mc-txt-number2">646 045 716</span># </li>
-    </ul>
-    <h3>Asynchronous Participation</h3>
+    <h2 id="m335intw">Task Force Meetings and Participation Channels</h2>
+    <p>Task Force meets regularly by teleconference, and plans to hold  face-to-face meetings 1-2 times a year in the future.</p>
+    <p>Updated information on LVTF meetings is on the <a href="https://www.w3.org/WAI/GL/low-vision-a11y-tf/wiki/Main_Page#Meetings">LVTF main wiki page, under "Meetings"</a>.</p>
+    <p><strong>IRC channel</strong>: <a href="irc://irc.w3.org/lvtf">#lvtf</a></p>
+    <p>Asynchronous participation:</p>
     <ul>
       <li><strong>Email</strong>: <a href="mailto:public-low-vision-a11y-tf@w3.org">public-low-vision-a11y-tf@w3.org</a> (public <a href="https://lists.w3.org/Archives/Public/public-low-vision-a11y-tf/">archive</a>)</li>
       <li><strong>Wiki</strong>: <a href="https://www.w3.org/WAI/GL/low-vision-a11y-tf/wiki/Main_Page">https://www.w3.org/WAI/GL/low-vision-a11y-tf/wiki/Main_Page</a></li>
       <li><strong>Surveys</strong>: <a href="https://www.w3.org/2002/09/wbs/81151/">https://www.w3.org/2002/09/wbs/81151/</a></li>
     </ul>
     <h2 id="about">About the Low Vision Accessibility Task Force</h2>
-    <p>The Low Vision Accessibility Task Force (LVTF) was created as a task force of the <a href="http://www.w3.org/WAI/GL/">Accessibility Guidelines Working Group (AG WG)</a> and, before it closure, the <a href="http://www.w3.org/WAI/UA/">User Agent Accessibility Guidelines Working Group (UAWG)</a>. The UAWG has closed.</p>
-    <p>The objective of the Task Force is to address web accessibility issues   specific to people with low vision.  Task Force work  includes:</p>
+    <p>The Low Vision Accessibility Task Force (LVTF) was created as a task force of the <a href="http://www.w3.org/WAI/GL/">Accessibility Guidelines Working Group (AG WG)</a> and the <a href="http://www.w3.org/WAI/UA/">User Agent Accessibility Guidelines Working Group (UAWG)</a>. The UAWG has closed.</p>
+    <p>The objective of the Task Force is to address web accessibility issues specific to people with low vision. Task Force work includes:</p>
     <ul>
       <li>Document accessibility requirements/needs for people with low vision</li>
       <li>Identify gaps between user needs and existing requirements in <a href="https://www.w3.org/WAI/intro/wcag">WCAG</a>, <a href="https://www.w3.org/WAI/intro/uaag">UAAG</a>, <a href="https://www.w3.org/WAI/intro/atag">ATAG</a></li>
-      <li>Document sufficient techniques, advisory techniques, and failures for WCAG 2.0 related to low vision</li>
-      <li>Develop an extension to WCAG 2.0 to address low vision</li>
-      <li>Refine <a href="http://www.w3.org/TR/UAAG20-Reference/">user agent use cases</a> for low vision, as needed</li>
+      <li>Document sufficient techniques, advisory techniques, and failures for WCAG 2 related to low vision</li>
+      <li>Develop supplemental guidance to address low vision beyond WCAG 2</li>
+      <li>Contribute to WCAG 3 requirements and guidance related to low vision</li>
+      <li>Potentially refine <a href="http://www.w3.org/TR/UAAG20-Reference/">user agent use cases</a> for low vision</li>
       <li>Develop support materials to explain the issues and implementation of the above resources effectively</li>
     </ul>
     <h3>Facilitators and Contacts</h3>
     <ul>
-      <li>Jim Allan, Facilitator</li>
-      <li>Andrew Kirkpatrick, Facilitator</li>
-      <li>Shawn Lawton Henry, W3C Staff Contact</li>
+      <li>Chris Loiselle, Co-Facilitator</li>
+      <li>Jonathan Avila, Co-Facilitator</li>
+      <li>Shawn Lawton Henry, W3C Team Contact</li>
     </ul>
     <h3>Work Statement</h3>
     <ul>
-      <li><a href="work-statement">Low Vision Accessibility Task Force Work Statement</a></li>
+      <li>Initial <a href="work-statement">Low Vision Accessibility Task Force Work Statement</a> (some details have changed since then)</li>
     </ul>
     <h3>Mailing List</h3>
     <p><a href="mailto:public-low-vision-a11y-tf@w3.org">public-low-vision-a11y-tf@w3.org</a> (public <a href="https://lists.w3.org/Archives/Public/public-low-vision-a11y-tf/">archive</a>)</p>
@@ -111,36 +104,35 @@
       <li>Keep comments within the scope of this group (see <a href="work-statement">Low Vision Accessibility Task Force Work Statement</a>).</li>
       <li>When raising an issue:
         <ul>
-          <li>provide a clear subject line including the topic it relates          to;</li>
-          <li>explain the issue briefly;</li>
-          <li>propose a brief &amp; specific solution;</li>
-          <li>for longer comments, put a summary at the beginning.</li>
+          <li>provide a clear subject line including the topic it relates          to</li>
+          <li>explain the issue briefly</li>
+          <li>propose a brief &amp; specific solution</li>
+          <li>for longer comments, put a summary at the beginning</li>
         </ul>
       </li>
       <li>When replying:
         <ul>
-          <li>keep your reply comments near the top of a message;</li>
-          <li>only quote essential parts of a previous message;</li>
-          <li>if there are multiple topics, break them into separate emails to          facilitate threading and to keep the subject line accurate.</li>
+          <li>keep your reply comments near the top of a message</li>
+          <li>only quote essential parts of a previous message</li>
+          <li>if there are multiple topics, break them into separate emails to          facilitate threading and to keep the subject line accurate</li>
         </ul>
       </li>
       <li>Please send any questions about the mailing list itself to the      Facilitators or Staff Contacts, not the whole list.</li>
       <li>No soliciting.</li>
     </ol>
     <h2 id="participation">Participation</h2>
-    <p>To join the Low Vision Accessibility Task Forces, individuals must be participants of the AG WG. Participants are expected to actively contribute to the work of LVTF, including:</p>
+    <p>To participate in the Low Vision Accessibility Task Force, individuals must  also be participants of the AG WG. (You don't have to have previously participated in AG WG. You do have to sign up to participate in AG WG in order to participate in LVTF).</p>
+    <p>Participants are expected to actively contribute to the work of LVTF, including:</p>
     <ul>
-      <li>Minimum 4 hours per week of LVTF work (this time  counts towards the individual's participation requirement in the AG WG);</li>
-      <li>Complete <a href="https://www.w3.org/2002/09/wbs/81151/all">LVTF surveys</a>;</li>
-      <li>Remain current on the <a href="https://lists.w3.org/Archives/Public/public-low-vision-a11y-tf/">LVTF mailing list</a> and respond in a timely manner to postings;</li>
-      <li>Participate in LVTF teleconferences, or record regrets in the <a rel="nofollow" href="https://www.w3.org/2002/09/wbs/81151/LVTF-telecons/">Availability for Upcoming LVTF Teleconferences Survey</a>.</li>
+      <li>Minimum 4 hours per week of LVTF work (this time  counts towards the individual's participation requirement in the AG WG)</li>
+      <li>Complete <a href="https://www.w3.org/2002/09/wbs/81151/all">LVTF surveys</a></li>
+      <li>Remain current on the <a href="https://lists.w3.org/Archives/Public/public-low-vision-a11y-tf/">LVTF mailing list</a> and respond in a timely manner to postings</li>
+      <li>Participate in LVTF teleconferences, or record regrets in the <a rel="nofollow" href="https://www.w3.org/2002/09/wbs/81151/LVTF-telecons/">Availability for Upcoming LVTF Teleconferences Survey</a></li>
     </ul>
-    <p>To become a participant of Low Vision Accessibility Task Force or with questions, contact <a href="mailto:akirkpat@adobe.com,jimallan@tsbvi.edu,shawn@w3.org?subject=Low Vision Accessibility Task Force Enquiry">Andrew Kirkpatrick, Jim Allan, and Shawn Henry</a></p>
-    <p><a href="https://www.w3.org/2000/09/dbwg/details?group=81151&amp;public=1">Current Low Vision A11Y TF participants </a></p>
-    <h3 id="patents">Patent Disclosures </h3>
-    <p>This Task Force is part of the <a href="http://www.w3.org/WAI/GL/charter">AG WG Charter</a> and <a href="http://www.w3.org/WAI/UA/2010/uawg_charter">UAWG Charter</a>, which operates under the <a href="http://www.w3.org/Consortium/Patent-Policy-20040205/">W3C Patent Policy</a> (5 February 2004 Version). W3C maintains a public list of any patent disclosures made in connection with the deliverables of the <a href="http://www.w3.org/2004/01/pp-impl/35422/status" rel="disclosure">AG WG</a> and the <a href="http://www.w3.org/2004/01/pp-impl/36791/status">UAWG</a>.</p>
-    <!--       <h2 id="additional"><a name="Additional" id="Additional" shape="rect"></a>Additional Information</h2> -->
-    <h2 id="timeline">&nbsp;</h2>
+    <p>To become a participant of Low Vision Accessibility Task Force or if you have questions, contact <a href="mailto:group-lvtf-plan@w3.org?subject=Low Vision Accessibility Task Force Enquiry">the Facilitators and Team contact via mailto:group-lvtf-plan@w3.org</a>.</p>
+    <p><a href="https://www.w3.org/groups/tf/low-vision-a11y-tf/participants">Low Vision Accessibility Task Force - Participants</a> lists curent and past participants.</p>
+    <h3 id="patents">Patent Disclosures</h3>
+    <p>This Task Force is part of the <a href="http://www.w3.org/WAI/GL/charter">AG WG Charter</a>, which operates under the <a href="https://www.w3.org/2019/12/ag-charter#patentpolicy">W3C Patent Policy</a>. W3C maintains a public list of any patent disclosures made in connection with the deliverables of the <a href="http://www.w3.org/2004/01/pp-impl/35422/status" rel="disclosure">AG WG</a>.</p>
   </div>
 </div>
 <div id="footer">
