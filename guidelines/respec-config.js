c9e2cfc16390ad3183dd0240bbb50dd5b47ae9eb
Michael Cooper <cooper@w3.org>
2021-05-07 10:29:07 -0400

    Various edits during May 2021 publication process
    Squashed commit of the following:
    
    commit bce04a574eb1cb0df18923706719105098b7bf61
    Author: Michael Cooper <cooper@w3.org>
    Date:   Fri May 7 10:28:07 2021 -0400
    
        fix note code
    
        fixes #1784
    
    commit 58fdf0c06884b2a7d13239644a447a33ab5e5921
    Author: Michael Cooper <cooper@w3.org>
    Date:   Wed May 5 10:36:22 2021 -0400
    
        add github param
    
    commit 20757a412da4df711d266400878779edabade2e9
    Author: Michael Cooper <cooper@w3.org>
    Date:   Fri Apr 23 09:54:28 2021 -0400
    
        more changelog cleanup
    
    commit 4e6d0eb7b6deface8a06b47e611f57fb9bcbe1b9
    Author: Michael Cooper <cooper@w3.org>
    Date:   Fri Apr 23 09:14:51 2021 -0400
    
        Update index.html
    
    commit fe0b3bc52bffe16a3cd24efb3c89dde7241c9c9c
    Author: Michael Cooper <cooper@w3.org>
    Date:   Fri Apr 23 09:10:38 2021 -0400
    
        Update change log for renamed SC
    
    commit 030fb4b1ee237e24844350142f9f01f031db3313
    Author: Michael Cooper <cooper@w3.org>
    Date:   Fri Apr 23 08:52:25 2021 -0400
    
        change respec profile
    
    commit 008ccb3405a1887e3961cc997f48c5b3f08079db
    Author: Michael Cooper <cooper@w3.org>
    Date:   Fri Apr 23 08:52:01 2021 -0400
    
        change respec to group option

--- guidelines/respec-config.js
+++ guidelines/respec-config.js
@@ -138,20 +138,9 @@ var respecConfig = {
 	// errata: 'https://www.w3.org/2010/02/rdfa/errata.html',
 	
 	// name of the WG
-	wg:           "Accessibility Guidelines Working Group",
-	
-	// URI of the public WG page
-	wgURI:        "https://www.w3.org/WAI/GL/",
-	
-	// name (with the @w3c.org) of the public mailing to which comments are due
-	wgPublicList: "public-agwg-comments",
-	
-	// URI of the patent status for this WG, for Rec-track documents
-	// !!!! IMPORTANT !!!!
-	// This is important for Rec-track documents, do not copy a patent URI from a random
-	// document unless you know what you're doing. If in doubt ask your friendly neighbourhood
-	// Team Contact.
-	wgPatentURI:  "https://www.w3.org/2004/01/pp-impl/35422/status",
+	group:           "ag",
+	github: "w3c/wcag",
+
 	maxTocLevel: 4,
 	
 	postProcess: [addTextSemantics, swapInDefinitions]
