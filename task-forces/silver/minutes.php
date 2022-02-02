aea312c2f50e5b33202a58826ffe60bfd97e5498
Michael Cooper <cooper@w3.org>
2022-02-02 08:39:25 -0500

    minutes tool

--- task-forces/silver/minutes.php
+++ task-forces/silver/minutes.php
@@ -1,73 +1,17 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-US" lang="en-US">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<link href="/WAI/wai-main.css" rel="stylesheet" type="text/css" />
-<!--[if IE 7]><link rel="stylesheet" href="/WAI/ie-old.css" type="text/css" media="all" /><![endif]-->
-<title>Minutes of the Cognitive A11Y TF</title>
-<style type="text/css">
-<!--
-.agenda {
-	line-height: normal;
-}
-.tight {
-	line-height: 100%;
-}
--->
-</style>
-</head>
-<body>
-<div id="controls">
-   <ul>
-      <li><a href="#skip"><img src="/Icons/downinpage.png" alt="" /> Skip to Content </a></li>
-      <li><a href="/WAI/changedesign.html"> | Change text size or colors </a> </li>
-   </ul>
-</div>
-<div id="masthead">
-   <p id="logos"><a href="http://www.w3.org/" title="W3C Home"><img alt="W3C logo" src="/Icons/w3c_home" /></a><a href="http://www.w3.org/WAI/" title="WAI Home"><img alt="Web Accessibility initiative" src="/WAI/images/wai-temp" /></a></p>
-</div>
-<div id="tagline">
-   <p>WAI: Strategies, guidelines, resources to make the Web accessible to people with disabilities</p>
-</div>
-<div id="nav">
-   <h2>Site Navigation</h2>
-   <p id="w3cnav"><a href="http://www.w3.org/">W3C Home</a></p>
-   <p id="subhomenav"><a href="/WAI/">Web Accessibility Initiative (WAI) Home</a></p>
-   <ol id="nav1">
-      <li><a href="http://www.w3.org/WAI/groups">WAI Groups</a></li>
-      <li class="current-nav"><a href="http://www.w3.org/WAI/grouptf">Group Task Forces</a> <ol type="1">
-					<li id="current-node" class="current-nav "><span class="label"><span id="current-icon">»</span><a href="./">Cognitive Accessibility Task Force</a></span> <ol type="i">
-						<!--<li class="external"><a href="@@">@@ TF Wiki <img src="/Icons/tr.png" alt="different format"/></a></li>-->
-						<li class="external">Minutes <img src="/Icons/tr.png" alt="different format" /></li>
-						<li class="external"><a href="work-statement">Work Statment</a></li>
-						<li class="external"><a href="https://www.w3.org/2000/09/dbwg/details?group=66524&amp;public=1">Current Participants <img src="/Icons/tr.png" alt="different format" /></a></li>
-						<li class="external"><a href="http://lists.w3.org/Archives/Public/public-cognitive-a11y-tf/">E-mail Archives <img src="/Icons/tr.png" alt="different format" /></a></li>
-						</ol> </li>
-					</ol> </li>
-   </ol>
-</div>
-<div id="main">
-   <div id="skipwrapper"> <a id="skip">-</a></div>
-   <h1>Minutes of the Silver TF</h1>
-	<p>This page shows minutes of the Silver Task Force only. Minutes of subgroups may be found on the <a href="/WAI/GL/WCAG3/minutes/">complete index of AG WG minutes</a>.</p>
-	<?php
-		$channel = "silver";
-		include "../../../../../2022/01/minutes/default-view.phi";
-		?>
-   <!-- end main -->
-</div>
-<div id="footer">
-   <h2>Document Information</h2>
-   <p class="status"><strong>Status:</strong> Updated $Date: 2019/02/28 14:47:44 $</p>
-   <div class="footer-nav">
-      <p>[<a href="http://www.w3.org/WAI/sitemap.html">WAI Site Map</a>] [<a href="http://www.w3.org/WAI/sitehelp.html">Help with WAI Website</a>] [<a href="http://www.w3.org/WAI/search.php">Search</a>] [<a href="/WAI/contacts">Contacting WAI</a>]<br clear="none" />
-         <strong>Feedback welcome to <a href="mailto:wai-eo-editors@w3.org">wai-eo-editors@w3.org</a></strong> (a publicly archived list) or <a href="mailto:wai@w3.org">wai@w3.org</a> (a WAI staff-only list).</p>
-   </div>
-   <div class="copyright">
-      <p><a rel="Copyright" href="/Consortium/Legal/ipr-notice#Copyright">Copyright</a> © 1994-2013 <a href="/"><acronym title="World Wide Web Consortium">W3C</acronym></a><sup>®</sup> (<a href="http://www.csail.mit.edu/"><acronym title="Massachusetts Institute of Technology">MIT</acronym></a>, <a href="http://www.ercim.org/"><acronym title="European Research Consortium for Informatics and Mathematics">ERCIM</acronym></a>, <a href="http://www.keio.ac.jp/">Keio</a>, <a href="http://ev.buaa.edu.cn/">Beihang</a>), All Rights Reserved. W3C <a href="/Consortium/Legal/ipr-notice#Legal_Disclaimer">liability</a>, <a href="/Consortium/Legal/ipr-notice#W3C_Trademarks">trademark</a>, <a rel="Copyright" href="/Consortium/Legal/copyright-documents">document use</a> and <a rel="Copyright" href="/Consortium/Legal/copyright-software">software licensing</a> rules apply. Your interactions with this site are in accordance with our <a href="/Consortium/Legal/privacy-statement#Public">public</a> and <a
-					href="/Consortium/Legal/privacy-statement#Members">Member</a> privacy statements.</p>
-   </div>
-   <!-- end footer -->
-</div>
-</body>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" dir="ltr">
+	<head>
+		<?php include "../../_head.phi"; ?>
+		<title>Minutes from Silver Task Force meetings</title>
+	</head>
+	<body>
+		<?php include "../../_header.phi"; ?>
+		<h1>Minutes from Silver Task Force meetings</h1>
+		<p>This page shows minutes of the Silver Task Force. Minutes for all groups working on WCAG 3 are available from <a href="/WAI/GL/WCAG3/minutes/">WCAG 3 Minutes</a>.</p>
+		<?php
+	$channel_id = "silver";
+	include "../../../../2022/01/minutes/default-view.phi";
+	?>
+		<?php include "../../_footer.phi"; ?>
+	</body>
 </html>
