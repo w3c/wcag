85967fe49e3a77a2420240f67a59e76f7c3db113
Michael Cooper <cooper@w3.org>
2022-03-28 17:08:29 -0400

    wcag2ict tf new location

---
+++ task-forces/wcag2ict/minutes.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" dir="ltr">
+	<head>
+		<?php include "../../_head.phi"; ?>
+		<title>Minutes from WCAG2ICT Task Force meetings</title>
+	</head>
+	<body>
+		<?php include "../../_header.phi"; ?>
+		<h1>Minutes from WCAG2ICT Task Force meetings</h1>
+		<?php
+	$channel_id = "wcag2ict";
+	include "../../../../2022/01/minutes/default-view.phi";
+	?>
+		<?php include "../../_footer.phi"; ?>
+	</body>
+</html>
---
+++ task-forces/wcag2ict/work-statement.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" dir="ltr">
+	<head>
+<?php include "../../_head.phi"; ?>
+		<title>Accessibility Guidelines WCAG2ICT Task Force Work Statement</title>
+	</head>
+	<body>
+<?php include "../../_header.phi"; ?>
+		<h1>WCAG2ICT Task Force Work Statement</h1>
+		<p>The WCAG2ICT Task Force is a task force of the <a href="https://www.w3.org/WAI/GL/">Accessibility Guidelines Working Group (AG WG)</a>. It assists this Working Group with the work identified below.</p>
+		<section id="status">
+			<h2>Status</h2>
+			<p>This task force is being reopened to update WCAG2ICT for WCAG 2.1 and 2.2. This is a draft work statement.</p>
+		</section>
+		<section id="objective">
+			<h2>Objective</h2>
+			<p>The objective of WCAG2ICT Task Force is to develop documentation describing how WCAG 2.1 &amp; 2.2 and its principles, guidelines, and success criteria could apply to non-Web Information and Communications Technologies (ICT). </p>
+			<p>This Task Force was formed to update  the existing document: “Guidance on Applying WCAG 2.0 to Non-Web Information and Communications Technologies (WCAG2ICT)” working group Note. Updates are needed to include new guidelines, principles and success criteria added in the WCAG 2.1 Recommendation as well as WCAG 2.2 will be a Recommendation before the updated Note is published. WCAG2ICT was developed to describe how to apply WCAG 2.0 and its principles, guidelines, and success criteria to non-web ICT (Information and Communications Technology) that are not web content.WCAG2ICT also plans to add new content that identifies problematic WCAG provisions when applied to non-Web ICT.</p>
+					</section>
+		<section id="scope">
+			<h2>Scope of Work</h2>
+			<p>The work of the task force includes: </p>
+		</section>
+		<ul>
+			<li>Determining how to apply each of the new WCAG 2.1 WCAG 2.2 guidelines and A, AA, and AAA success criteria to non-Web ICT; (this includes provision of additional details for application to closed product software</li>
+			<li>Producing an updated Working Group Note (a form of W3C Technical Report)—under the coordination of the AG WG, with publication of TF materials as reviewed and approved by the AG WG—that describes:
+			<ul>
+				<li>how each of the new WCAG 2.1 and 2.2 A and AA success criteria could apply to non-Web ICT;</li>
+				<li>how each of the new WCAG 2.1 and 2.2 principles and guidelines could apply to non-Web ICT;</li>
+				<li>clarification of challenges of applying particular  WCAG 2.2 success criteria could apply to non-web technologies, including closed product software</li>
+				<li>address challenges in WCAG open issues *** provide links***</li>
+			</ul></li>
+		</ul>
+		<p>Explicitly out of scope for the WCAG2ICT Task Force work:</p>
+		<ul>
+			<li>Proposing changes to WCAG 2.2;</li>
+			<li>Developing techniques or interpretations of WCAG 2.2 for implementing WCAG 2.2 in Web technologies;</li>
+			<li>Developing any specific, sufficient techniques, including platform-specific techniques, for implementing WCAG 2.2 in non-Web ICT, though examples may be in scope;</li>
+			<li><a>Determining whether WCAG 2.2 provisions <i>should be</i> applied to non-web content;</a> </li>
+			<li>Developing a normative standard.</li>
+		</ul>
+		<section id="approach">
+			<h2>Approach</h2>
+			<p>The primary effort of this Task Force will be describing how to interpret WCAG 2.2 and its principles, guidelines, success criteria and conformance requirements when applying them to non-web ICT, including all WCAG 2.2 A and AA success criteria; and evaluating what WCAG Conformance means in the context of non-Web ICT. The resulting Working Group Note will include a success-criteria-by-success-criteria discussion of applying WCAG 2.2 to non-Web ICT, including their interface components and platforms, and the extent to which <a href="http://www.w3.org/TR/WCAG20/#conformance">WCAG Conformance</a> is meaningful to non-Web ICT.</p>
+			<p>The work may be broken down into ad hoc sub-teams if and as necessary, under the coordination of the Task Force facilitator(s), to efficiently develop the core Working Group Note.</p>
+			<p>The work will be carried out through a series of document drafts, all publicly visible, with opportunities for public comment through publicly available comment lists. In particular, key stakeholders such as developers, evaluators, accessibility experts, researchers, procurement experts, and end users will be regularly involved in the development process. Key participants will be drawn from people with expertise not only in Web technologies, <a>but also those with platform-specific accessibility expertise, including people involved in the development of accessibility frameworks, accessible platforms, accessible user interface components, and accessible applications for a broad variety of computing environments.</a></p>
+		</section>
+		<section id="liaisons">
+			<h2>Liaisons</h2>
+			<p>The following proposed participating liaisons may change pending confirmation, and/or be expanded:</p>
+			<ul>
+				<li><a>US CIO Council, Accessibility Committee</a></li>
+				<li><a href="http://www.etsi.org/">ETSI</a></li>
+				<li>Other?</li>
+			</ul>
+		</section>
+		<section id="communication">
+			<h2>Communication</h2>
+			<p>WCAG2ICT Task Force communications are publicly visible. Communication mechanisms for the WCAG2ICT Task Force include:</p>
+			<ol start="1" type="1">
+				<li>WCAG2ICT Task Force mailing list, mailing list archive, and GitHub issue tracker;</li>
+				<li><a href="https://www.w3.org/WAI/GL/WCAG2ICT-TF">WCAG2ICT subpage</a> off of the <a href="http://www.w3.org/WAI/GL/">AG Working Group</a> home page; (to be updated)</li>
+				<li>Weekly (and at times twice-weekly) teleconferences, with teleconference minutes also sent to WCAG WG mailing list;</li>
+				<li>Regular updates and feedback to AG WG, and to <a href="http://www.w3.org/WAI/CG/">WAI Coordination Group</a> (WAI CG) as needed;</li>
+				<li>Regular submission of deliverables to AG WG for approval;</li>
+				<li>Summary of current status and progress on the <a href="https://www.w3.org/WAI/GL/WCAG2ICT-TF">WCAG2ICT Task Force home page</a>.</li>
+			</ol>
+		</section>
+		<section id="participation">
+			<h2>Participation</h2>
+			<p>WCAG2ICT Task Force participants must join the Task Force through the AG WG. Task Force participants must agree to actively contribute to the work of WCAG2ICT Task Force, including:</p>
+			<ul>
+				<li>Minimum 4 hours per week of WCAG2ICT Task Force work through the end of the project, estimated to complete by <a>Dec. 2022</a> ;</li>
+				<li>Time contributed on WCAG2ICT Task Force counts towards AG WG participation requirement;</li>
+				<li>Remain current on the WCAG2ICT TF mailing list and respond in a timely manner to postings;</li>
+				<li>Participate in WCAG2ICT Task Force telephone meetings.</li>
+			</ul>
+			<p><a href="https://www.w3.org/2000/09/dbwg/details?group=94845&amp;public=1">Current WCAG2ICT Task Force participants</a></p>
+		</section>
+		<section id="facilitation">
+			<h2>Facilitation</h2>
+			<p>Staff contacts from the Accessibility Guidelines Working Group oversee attention to W3C Process with respect to the chartered requirements of the Working Group. The facilitators set agenda, lead meetings, determine consensus, and are the primary liaison to the Working Group.</p>
+			<ul>
+				<li><strong>Facilitators:</strong> TBD</li>
+				<li><strong>Technical Staff Contact:</strong> <a href="https://www.w3.org/People/Brewer/">Judy Brewer</a></li>
+				<li><strong>Adminstrative Staff Contact:</strong> <a href="http://www.w3.org/People/cooper/">Michael Cooper</a></li>
+			</ul>
+		</section>
+		<section id="patentpolicy">
+			<h2>Patent Policy</h2>
+			<p>This Task Force is part of the <a href="https://www.w3.org/WAI/GL/charter">AG Working Group Charter</a>, which operates under the <a href="https://www.w3.org/Consortium/Patent-Policy-20040205/">W3C Patent Policy</a> (5 February 2004 Version). W3C maintains a public list of any patent disclosures made in connection with the deliverables of the <a href="https://www.w3.org/2004/01/pp-impl/35422/status">AG Working Group</a>.</p>
+		</section>
+
+<?php include "../../_footer.phi"; ?>
+	</body>
+</html>
