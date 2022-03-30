e41b4bae45c413fbdc5a4bea113ab31ec1cdef67
Michael Cooper <cooper@w3.org>
2022-03-30 07:30:48 -0400

    coga update

--- task-forces/coga/work-statement.php
+++ task-forces/coga/work-statement.php
@@ -2,7 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en" dir="ltr">
 	<head>
 <?php include "../../_head.phi"; ?>
-		<title>Coga Task Force Work Statement</title>
+		<title>Cognitive and Learning Disabilities Accessibility Task Force Work Statement</title>
 	</head>
 <body>
 <?php include "../../_header.phi"; ?>
@@ -11,80 +11,90 @@
 
 <div id="main">
 	<h1>Cognitive and Learning Disabilities Accessibility Task Force (Coga TF) Work Statement</h1>
-	<p>The Cognitive and Learning Disabilities Accessibility Task Force is a task force of the <a href="http://www.w3.org/WAI/APA/">Accessible Platform Architectures (APA) Working Group</a> and the <a href="http://www.w3.org/WAI/GL/">Accessibility Guidelines Working Group (AG WG)</a>. It assists these Working Groups with the work identified below.</p>
+	<p>The Cognitive and Learning Disabilities Accessibility Task Force is a task force of the <a href="http://www.w3.org/WAI/APA/">Accessible Platform Architectures (APA) Working Group</a> and the <a href="http://www.w3.org/WAI/GL/">Accessibility Guidelines Working Group (AG WG)</a>. </p>
 	<div id="contents">
 		<h2>Page Contents</h2>
 				<ul>
-					<li><a href="#status">Status</a></li>
+					<li><a href="#Background">Background</a></li>
 					<li><a href="#objective">Objective</a></li>
-					<li><a href="#scope">Scope of Work</a></li>
 					<li><a href="#approach">Approach</a></li>
-					<li><a href="#timeline">Timeline</a></li>
-					<li><a href="#dependencies">Dependencies</a></li>
-					<li><a href="#communication">Communication</a></li>
-					<li><a href="#participation">Participation </a></li>
-					<li><a href="#patentpolicy">Patent Policy</a></li>
+					<li><a href="#References">References</a></li>
 				</ul></div>
 				<div>
-					<h2 id="status">Status</h2> <p> This is a Work Statement for the Cognitive A11Y TF. It has been approved by the AG WG and the APA WG. Recruiting is beginning and the Task Force will soon be active. </p></div>
-				<div>
-					<h2 id="objective">Objective</h2> <p>The objective of the Cognitive and Learning Disabilities Accessibility Task Force is to improve Web accessibility for people with cognitive and learning disabilities. This will begin with research and gap analysis. Then the group will develop draft proposed guidance and techniques to make web content, content authoring, and user agent implementation accessible and more usable by people with cognitive and learning disabilities. It will also review existing techniques and consider ways to improve them, and build new techniques where necessary.</p></div>
-				<div>
-				<h2 id="approach">Approach</h2><p>The Cognitive A11Y TF will:</p> <ul>
-		<li>Identify related work inside and outside of the W3C;</li>
-					<li>Review existing techniques and propose new features where needed;</li>
-		<li>Gather information on different related technologies and techniques including where techniques in support of cognitive and learning disabled users may conflict with what is needed for other users;</li>
-		<li>Build a repository of related research into cognition and disabilities;</li>
-		<li>Build a  use case repository to describe different cognitive disabilities and scenarios for how persons with atypical ability profiles use technology successfully or unsuccessfully;</li>
-		<li>Make predictions of where technology is headed;</li>
-		<li>Identify stakeholders, from whom the task force can solicit ongoing feedback;</li>
-		<li>Document business cases and identify areas of resistance, risks and opportunities. </li>
-		</ul><p>The task force expects to produce some testable authoring techniques, but also expects to produce techniques that are not testable. Tests and feedback will help ensure techniques give real benefits to end users and can be used by authors, tool providers, browsers, and assistive technologies. </p><p>The task force plans to produce the following deliverables:</p> <ul>
-		<li>Gap analysis;</li>
-		<li>Roadmap document;</li>
-		<li>Techniques for Web content authors. </li>
-		</ul>
-				<p>The Task Force may form subteams to carry out work of the Task Force.</p></div>
-				<div><h2 id="timeline">Timeline</h2>
-				<p>The following presents a rough timeline of the work:</p>
-				<p>TBD</p></div>
-				<div><h2 id="dependencies">Dependencies</h2>
-
-					<p>The Cognitive and Learning Disabilities Accessibility Task Force has the following dependencies:</p>
+					<h2 id="Background">Background</h2> 
+					<p> Our work includes <a href="https://www.w3.org/TR/coga-usable">Making Content Usable for People with Cognitive and Learning Disabilities</a>. This document is for people who are involved in making web content, applications, and digital content. </p>
+					<p>The Task Force (TF) is currently recruiting people to help with:</p>
 					<ul>
-						<li><a href="http://www.w3.org/WAI/GL/">AGWG</a> and <a href="http://www.w3.org/WAI/APA/">APA WG</a> to utilize this group's previous and current work that addresses the cognitive accessibility space;</li>
-						<li><a href="http://www.w3.org/WAI/GL/">AG WG</a> to ensure accurate interpretation and representation of WCAG requirements;</li>
-						<li> <a href="http://www.w3.org/WAI/GL/wcag-pf-html5-task-force">HTML5 and ARIA Techniques Task Force</a> to leverage the development of HTML 5 and ARIA techniques for cognitive accessibility.</li>
+						<li>literary reviews and research relating to web accessibility for mental health,</li>
+						<li>creating cognitively accessible deliverables, both text, audio, images, and other multimedia, and</li>
+						<li>understanding our user groups (people with learning and cognitive disabilities and mental health related disabilities).</li>
 					</ul>
-					<p>The Cognitive and Learning Disabilities Accessibility Task Force will also coordinate with the <a href="http://www.w3.org/WAI/EO/">Education and Outreach Working Group (EOWG)</a> where necessary to improve the writing, presentation, and educational value of the developed materials.</p>
+					<p>More details about our work, and how to work with us, are available on our <a href="https://www.w3.org/WAI/GL/task-forces/coga/wiki/Main_Page">COGA wiki</a>.</p>
+				
+				
 				</div>
 				<div>
-				<h2 id="communication">Communication</h2>
-				<p>The Cognitive and Learning Disabilities Accessibility Task Force communications are publicly visible. Communication mechanisms for the Task Force will include:</p>
+					<h2 id="objective">Objective</h2> 
+					<p>The objective of the Cognitive and Learning Disabilities Accessibility Task Force is to improve accessibility of web content, applications, and digital content for people with cognitive disabilities and people with learning disabilities.</p>
+					<p><strong>This includes:</strong></p>
+					<ul>
+						<li>cognitive disabilities,</li>
+						<li>learning disabilities (LD),</li>
+						<li>neurodiversity,</li>
+						<li>intellectual disabilities,</li>
+						<li>age-related memory loss,</li>
+						<li>a subset of mental health conditions that have clear research that can be integrated into Making Content Usable,</li>
+						<li>and specific learning disabilities (SLD). [<a href="#References">Making Content Usable 2.4, Language Use</a>]</li>
+					</ul>
+					<p><strong>Our work includes:</strong></p>
+					<ul>
+						<li>research and analysis,</li>
+						<li>developing guidance and techniques to make web content, content authoring, and user agents accessible and more usable by people with cognitive and learning disabilities, and</li>
+						<li>review existing techniques and consider ways to improve them, and build new techniques where necessary.</li>
+					</ul>
+					<p>We will consider mental health conditions and how we can best integrate them into our work.</p>
+				
+				</div>
+				<div>
+				<h2 id="approach">Approach</h2>
+				<p>The work of the COGA TF includes:</p> 
 				<ul>
-					<li>Cognitive and Learning Disabilities Accessibility Task Force mailing list and <a href="http://lists.w3.org/Archives/Public/public-cognitive-a11y-tf/">mailing list archive</a>;</li>
-					<li>Use of a <a href="wiki/">wiki</a> to share development of resources;</li>
-					<li><a href="https://www.w3.org/2002/09/wbs/67703/">Web-based surveys</a> to poll group opinion;</li>
-					<li><a href="http://www.w3.org/WAI/PF/cognitive-a11y-tf/track/">Issue Tracker</a> to manage issues and actions;</li>
-					<li>IRC discussion on the <a href="irc://irc.w3.org/coga">#coga</a> IRC channel, used largely for minute-taking;</li>
-					<li>Weekly teleconferences, <a href="minutes">minutes</a> from the teleconferences are also sent to AG WG and APA WG mailing lists;</li>
-					<li>Regular updates and feedback to the AG WG and APA WG, and potentially the <a href="http://www.w3.org/WAI/CG/">WAI Coordination Group (WAI CG)</a> as needed;</li>
-					<li>Regular submission of deliverables to the AG WG and APA WG for approval;</li>
-					<li>Summary of current status and progress on the <a href="./">Cognitive and Learning Disabilities Accessibility Task Force home page</a>.</li>
-				</ul>
-			</div>
-			<div><h2 id="participation">Participation</h2>
-				<p>To join the Cognitive A11Y TF, individuals must be participants of either the AG WG or the APA WG. Participants are expected to actively contribute to the work of Cognitive A11Y TF, including:</p>
+						<li>Performing research and analysis with the intent of publishing;</li>
+						<li>Creating guidance for Web content authors;</li>
+						<li>Creating guidance for creators of applications and platforms;</li>
+						<li>Co-creating guidelines with the Silver TF for WCAG 3.0;</li>
+						<li>Helping other working groups in the W3C have more inclusive deliverables;</li>
+						<li>Identifying related work inside and outside of the W3C;</li>
+						<li>Reviewing existing techniques and propose new features where needed;</li>
+						<li>Gathering information on different related technologies and techniques including where techniques in support of users with cognitive and learning disabilities may conflict with what is needed for other users;</li>
+						<li>Building a use case repository to describe different cognitive disabilities and scenarios for how persons with atypical ability profiles use technology successfully and how failures may occur;</li>
+						<li>Making predictions of how emerging technologies might be used to improve (or even potentially hinder) cognitive accessibility;</li>
+						<li>Documenting business cases and identify areas of resistance, risks and opportunities.</li>
+					</ul>
+			
+				<p>The Task Force expects to produce quantitatively testable authoring techniques, but also expects to produce techniques that are not testable by automated checkers and must involve user testing. Tests and feedback will help ensure techniques give real benefits to end users and can be used by authors, tool providers, browsers, and assistive technologies.</p>
+				<p>The Task Force plans to produce the following deliverables:</p>
 				<ul>
-					<li>Minimum 4 hours per week of Coga TF work (this time also counts towards the individual's participation requirement in the sponsoring WG through which they have joined);</li>
-					<li>Remain current on the Cognitive and Learning Disabilities Accessibility Task Force mailing list and respond in a timely manner to postings;</li>
-					<li>Participate in Cognitive and Learning Disabilities Accessibility Task Force telephone meetings, or send regrets to the Cognitive and Learning Disabilities Accessibility Task Force mailing list.</li>
-				</ul>
-				<p>Contact <a href="mailto:cooper@w3.org">Michael Cooper</a> to become a participant of Cognitive and Learning Disabilities Accessibility Task Force or with questions.</p></div> <div><h3 id="leadership">Leadership</h3> <p>Staff contacts oversee attention to W3C Process with respect to the chartered requirements. The Facilitator(s) set agenda, lead meetings, determine consensus, and are the primary liaison to the WGs.</p> <ul>
-					<li><strong>Co-Facilitators:</strong> Lisa Seeman, second to be determined</li>
-		<li><strong>AGWG and APA WG Staff Contact: </strong><a href="http://www.w3.org/People/cooper/">Michael Cooper</a></li>
-		</ul> </div> <div> <h3 id="participants">Participants</h3> <p><a href="https://www.w3.org/2000/09/dbwg/details?group=67703&amp;public=1">Current Cognitive A11Y TF participants </a></p> </div> <div> <h2 id="patentpolicy">Patent Policy</h2>
-				<p> This Task Force is part of the <a href="http://www.w3.org/WAI/GL/charter">AG WG Charter</a> and <a href="http://www.w3.org/WAI/APA/charter">APA WG Charter</a>, which operates under the <a href="http://www.w3.org/Consortium/Patent-Policy-20040205/">W3C Patent Policy</a> (5 February 2004 Version). W3C maintains a public list of any patent disclosures made in connection with the deliverables of the <a href="https://www.w3.org/2004/01/pp-impl/35422/status">AGWG</a> and the <a href="https://www.w3.org/2004/01/pp-impl/83907/status" rel="disclosure">APA WG</a>.</p> </div> </div>
+						<li>Making Content Usable (second draft),</li>
+						<li>Research module/ literary review and/or issues papers,</li>
+						<li>Co-created guidelines with the Silver TF for WCAG 3.0.</li>
+					
+					</ul>
+
+					<p>The Task Force may form subteams to carry out work of the Task Force. See our wiki <a href="https://www.w3.org/WAI/GL/task-forces/coga/wiki/Subgroups">Subgroups page</a> for more details, including subteam work statements.</p>
+				</div>
+				<div><h2 id="References">References</h2>
+
+					
+					<ul>
+						<li><a href="https://rawgit.com/w3c/coga/master/issue-papers/">Cognitive Accessibility Issue Papers Editor’s Draft</a></li>
+						<li><a href="https://rawgit.com/w3c/coga/master/gap-analysis/">Cognitive Accessibility Roadmap and Gap Analysis</a></li>
+						<li> <a href="https://w3c.github.io/coga/user-research/">Cognitive Accessibility User Research Editor’s Draft</a></li>
+						<li> <a href="https://www.w3.org/TR/coga-usable/#language-use">Making content usable 2.4, Language Use</a></li>
+					</ul>
+					
+				</div>
+			
 		<?php include "../../_footer.phi"; ?>
 
 </body>
