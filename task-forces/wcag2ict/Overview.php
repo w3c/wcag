41ea03db1ad3e637418d7fad6f1d69d781486012
Michael Cooper <cooper@w3.org>
2022-05-03 14:27:05 -0400

    catch up to cvs

--- task-forces/wcag2ict/Overview.php
+++ task-forces/wcag2ict/Overview.php
@@ -1,80 +1,80 @@
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-US" lang="en-US">
-<head>
-	<?php include "../../_head.phi"; ?>
-	<title>WCAG2ICT Task Force</title>
-</head>
-
-<body>
-	<?php include "../../_header.phi"; ?>
-	<h1>WCAG2ICT Task Force</h1>
-	<section id="communication">
-		<h2>Meetings and Communication</h2>
-		<p>The WCAG2ICT TF conducts its work using a variety of synchronous and asynchronous tools. Most work is done via email and issues as shown below, supplemented by occasional teleconferences.</p>
-		<ul>
-			<li><a href="#email">Email lists</a>;</li>
-			<li><a href="wiki/">wiki</a>;</li>
-			<li><a href="https://github.com/w3c/wcag2ict/">wcag2ict GitHub repository</a> and <a href="https://w3c.github.io/wcag2ict/">GitHub draft documents</a>;</li>
-			<li>IRC discussion on the <a href="irc://irc.w3.org/wcag2ict">#wcag2ict</a> IRCchannel, used largely for minute-taking;</li>
-			<li><a href="/2002/09/wbs/55145/">Web-Based Surveys (WBS)</a>;</li>
-			<li>Teleconferences of the task force (also see <a href="minutes">meeting minutes</a>);</li>
-			<li>Face to face meetings;</li>
-		</ul>
-		<p>These tools are used by participants of the Task Force. For ways non-participants can contribute, see how to <a href="../../participation">participate in the Working Group</a> and <a href="../../../WCAG20/comments/">file comments</a>.</p>
-		
-		<section id="teleconferences">
-			<h3>Teleconferences</h3>
+	<head>
+		<?php include "../../_head.phi"; ?>
+		<title>WCAG2ICT Task Force</title>
+	</head>
+	
+	<body>
+		<?php include "../../_header.phi"; ?>
+		<h1>WCAG2ICT Task Force</h1>
+		<section id="communication">
+			<h2>Meetings and Communication</h2>
+			<p>The WCAG2ICT TF conducts its work using a variety of synchronous and asynchronous tools. Most work is done via email and issues as shown below, supplemented by occasional teleconferences.</p>
 			<ul>
-				<?php
+				<li><a href="#email">Email lists</a>;</li>
+				<li><a href="wiki/">wiki</a>;</li>
+				<li><a href="https://github.com/w3c/wcag2ict/">wcag2ict GitHub repository</a> and <a href="https://w3c.github.io/wcag2ict/">GitHub draft documents</a>;</li>
+				<li>IRC discussion on the <a href="irc://irc.w3.org/wcag2ict">#wcag2ict</a> IRCchannel, used largely for minute-taking;</li>
+				<li><a href="/2002/09/wbs/55145/">Web-Based Surveys (WBS)</a>;</li>
+				<li>Teleconferences of the task force (also see <a href="minutes">meeting minutes</a>);</li>
+				<li>Face to face meetings;</li>
+			</ul>
+			<p>These tools are used by participants of the Task Force. For ways non-participants can contribute, see how to <a href="../../participation">participate in the Working Group</a> and <a href="../../../WCAG20/comments/">file comments</a>.</p>
+			
+			<section id="teleconferences">
+				<h3>Teleconferences</h3>
+				<ul>
+					<?php
 					include "../../../../2017/01/telecon-info/filtered-telecon-list.phi";
 					showTeleconList("wcag2ict");
 				?>
-			</ul>
+				</ul>
+			</section>
+			<section id="minutes">
+				<h3>Meeting Minutes</h3>
+				<p><a href="minutes">Minutes from previous meetings</a> are available.</p>
+			</section>
+			<section id="email">
+				<h3>Mailing Lists</h3>
+				<p>The WCAG2ICT TF uses the public-wcag2ict-tf@w3.org mailing list (<a href="http://lists.w3.org/Archives/Public/public-wcag2ict-tf/">mailing list archives</a>) for email discussion; members of the public can post to this list to send input to the task force.</p>
+			</section>
 		</section>
-		<section id="minutes">
-			<h3>Meeting Minutes</h3>
-			<p><a href="minutes">Minutes from previous meetings</a> are available.</p>
-		</section>
-		<section id="email">
-			<h3>Mailing Lists</h3>
-			<p>The WCAG2ICT TF uses the public-wcag2ict-tf@w3.org mailing list (<a href="http://lists.w3.org/Archives/Public/public-wcag2ict-tf/">mailing list archives</a>) for email discussion; members of the public can post to this list to send input to the task force.</p>
-		</section>
-	</section>
-	
-	<section id="work">
-		<h2>Current Work</h2>
-
-	</section>
-	
-	<section id="publications">
-		<h2>Publications</h2>
-	</section>
-	
-	<section id="contribute">
-		<h2>How to Comment, Contribute, and Participate</h2>
-		<p>To join the WCAG2ICT TF, individuals must be participants of the Accessibility Guidelines Working Group. Participants are expected to <a href="work-statement#participation">actively contribute</a> to the work of the Task Force. If you are interested in participating in the Silver TF, please send e-mail to @@ and include a little bit about what you’re interested in and how you think that you may be able to contribute to the Task Force. Then follow the <a href="../../participation">participation procedures of the Accessibility Guidelines Working Group</a>, and once you have joined ask <a href="mailto:cooper@w3.org">Michael Cooper</a> to add you to the task force.</p>
-		<p><a href="https://www.w3.org/groups/tf/wcag2ict/participants">Current participants in the WCAG2ICT TF</a>.</p>
 		
-	</section> 
-	
-	<section id="administrative">
-		<h2>Administrative Information</h2>
-		<p>The WCAG2ICT TF is a Task Force of the <a href="http://www.w3.org/WAI/GL/">Accessible Guidelines Working Group (AG WG)</a>.</p>
-		<section id="facilitator">
-			<h3>Facilitator and Contacts</h3>
-			<ul>
-				<li><strong>Facilitators:</strong> TBD</li>
-				<li><strong>Technical Staff Contact:</strong> <a href="https://www.w3.org/People/Brewer/">Judy Brewer</a></li>
-				<li><strong>Adminstrative Staff Contact:</strong> <a href="http://www.w3.org/People/cooper/">Michael Cooper</a></li>
-			</ul>
-			<p>Contact the task force leadership and working group chairs by email to <a href="mailto:group-ag-chairs@w3.org">group-ag-chairs@w3.org</a></p>
+		<section id="work">
+			<h2>Current Work</h2>
+			
 		</section>
-		<section id="work-statement">
-			<h3>Work Statement</h3>
-			<p>The <a href="work-statement">WCAG2ICT Task Force Work Statement</a> defines the initial objective, scope, approach, and participation of the Task Force.</p>
+		
+		<section id="publications">
+			<h2>Publications</h2>
 		</section>
-	</section>
-	
-	<?php include "../../_footer.phi"; ?>
-</body>
+		
+		<section id="contribute">
+			<h2>How to Comment, Contribute, and Participate</h2>
+			<p>To join the WCAG2ICT TF, individuals must be participants of the Accessibility Guidelines Working Group. Participants are expected to <a href="work-statement#participation">actively contribute</a> to the work of the Task Force. If you are interested in participating in the Silver TF, please send e-mail to @@ and include a little bit about what youâ€™re interested in and how you think that you may be able to contribute to the Task Force. Then follow the <a href="../../participation">participation procedures of the Accessibility Guidelines Working Group</a>, and once you have joined ask <a href="mailto:cooper@w3.org">Michael Cooper</a> to add you to the task force.</p>
+			<p><a href="https://www.w3.org/groups/tf/wcag2ict/participants">Current participants in the WCAG2ICT TF</a>.</p>
+			
+		</section> 
+		
+		<section id="administrative">
+			<h2>Administrative Information</h2>
+			<p>The WCAG2ICT TF is a Task Force of the <a href="http://www.w3.org/WAI/GL/">Accessible Guidelines Working Group (AG WG)</a>.</p>
+			<section id="facilitator">
+				<h3>Facilitator and Contacts</h3>
+				<ul>
+					<li><strong>Facilitators:</strong> TBD</li>
+					<li><strong>Technical Staff Contact:</strong> <a href="https://www.w3.org/People/Brewer/">Judy Brewer</a></li>
+					<li><strong>Adminstrative Staff Contact:</strong> <a href="http://www.w3.org/People/cooper/">Michael Cooper</a></li>
+				</ul>
+				<p>Contact the task force leadership and working group chairs by email to <a href="mailto:group-ag-chairs@w3.org">group-ag-chairs@w3.org</a></p>
+			</section>
+			<section id="work-statement">
+				<h3>Work Statement</h3>
+				<p>The <a href="work-statement">WCAG2ICT Task Force Work Statement</a> defines the initial objective, scope, approach, and participation of the Task Force.</p>
+			</section>
+		</section>
+		
+		<?php include "../../_footer.phi"; ?>
+	</body>
 </html>
