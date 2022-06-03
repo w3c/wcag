3f7a35a3f3e830537570c35e9aefc20528457288
maryjom <maryjom@us.ibm.com>
2022-06-03 19:17:45 -0400

    WCAG2ICT Overview page corrections

--- task-forces/wcag2ict/Overview.php
+++ task-forces/wcag2ict/Overview.php
@@ -9,22 +9,41 @@
 	<?php include "../../_header.phi"; ?>
 	<h1>WCAG2ICT Task Force</h1>
 	<p>The objective of the WCAG2ICT Task Force is to develop documentation describing how WCAG 2.1 and 2.2 and its principles, guidelines, and success criteria could apply to non-Web Information and Communications Technologies (ICT). </p>
+	<section id="contents">
+		<h2>Page Contents</h2>
+		<ul>
+			<li><a href="#announcements">Announcements</a></li>
+			<li><a href="#communication">Meetings and Communication</a></li>
+			<li><a href="#work">Current Work</a></li>
+			<li><a href="#publications">Publications</a></li>
+			<li><a href="#contribute">How to Comment, Contribute, and Participate</a></li>
+			<li><a href="#administrative">Administrative Information</a></li>
+		</ul>
+	</section>
+	<section id="announcements">
+			<h2>Announcements</h2>
+			<p>No announcements at the moment.</p>
+	</section>
 	<section id="communication">
 		<h2>Meetings and Communication</h2>
-		<p>The WCAG2ICT TF conducts its work using a variety of synchronous and asynchronous tools. Most work is done via email and issues as shown below, supplemented by occasional teleconferences.</p>
+		<p>The WCAG2ICT TF conducts its work using a variety of synchronous and asynchronous tools. Once the TF is re-established, its work will be done via teleconference, email and GitHub issues.</p>
 		<ul>
 			<li><a href="#email">Email lists</a>;</li>
-			<li><a href="wiki/">wiki</a>;</li>
 			<li><a href="https://github.com/w3c/wcag2ict/">wcag2ict GitHub repository</a> and <a href="https://w3c.github.io/wcag2ict/">GitHub draft documents</a>;</li>
-			<li>IRC discussion on the <a href="irc://irc.w3.org/wcag2ict">#wcag2ict</a> IRCchannel, used largely for minute-taking;</li>
+			<li>IRC options</li>
+			<ul>
+				<li>IRC discussion on the <li><a href="http://irc.w3.org/?channels=#wcag2ict">#wcag2ict</a> IRC channel, used largely for minute-taking;</li>
+				<li>Server: irc.w3.org; Port 6665, 6667, 21, or 994; Channel: #wcag2ict</li>
+			</ul>	
 			<li><a href="/2002/09/wbs/55145/">Web-Based Surveys (WBS)</a>;</li>
-			<li>Teleconferences of the task force (also see <a href="minutes">meeting minutes</a>);</li>
+			<li><a href="#teleconferences">Teleconferences of the task force</a> (also see <a href="minutes">meeting minutes</a>);</li>
 			<li>Face to face meetings;</li>
 		</ul>
-		<p>These tools are used by participants of the Task Force. For ways non-participants can contribute, see how to <a href="../../participation">participate in the Working Group</a> and <a href="../../../WCAG20/comments/">file comments</a>.</p>
+		<p>These tools are used by participants of the Task Force. For ways you can contribute, see <a href="#contribute">How to Comment, Contribute, and Participate</a>.</p>
 		
 		<section id="teleconferences">
 			<h3>Teleconferences</h3>
+			<p>A teleconference schedule will be created when the WCAG2ICT Task Force is officially re-established.</p>
 			<ul>
 				<?php
 					include "../../../../2017/01/telecon-info/filtered-telecon-list.phi";
@@ -38,7 +57,7 @@
 		</section>
 		<section id="email">
 			<h3>Mailing Lists</h3>
-			<p>The WCAG2ICT TF uses the public-wcag2ict-tf@w3.org mailing list (<a href="http://lists.w3.org/Archives/Public/public-wcag2ict-tf/">mailing list archives</a>) for email discussion. Members of the public can post to this list to send input to the task force.</p>
+			<p>The WCAG2ICT TF uses the public-wcag2ict-tf@w3.org mailing list (<a href="http://lists.w3.org/Archives/Public/public-wcag2ict-tf/">mailing list archives</a>) for email discussion. Participants are automatically added to this mailing list when they become a participant of the Task Force. Members of the public can post to this list to send input to the task force.</p>
 		</section>
 	</section>
 	
@@ -49,13 +68,13 @@
 	
 	<section id="publications">
 		<h2>Publications</h2>
-		<p><a href="https://www.w3.org/TR/wcag2ict/">Formal publication of Applying WCAG 2.x to Non-Web Information and Communications Technologies (WCAG2ICT)</a></p>
+		<p>W3C Working Group Note <a href="https://www.w3.org/TR/wcag2ict/">Applying WCAG 2.x to Non-Web Information and Communications Technologies (WCAG2ICT)</a></p>
 	</section>
 	
 	<section id="contribute">
 		<h2>How to Comment, Contribute, and Participate</h2>
 		<p>To join the WCAG2ICT TF, individuals must be participants of the Accessibility Guidelines Working Group. Participants are expected to <a href="work-statement#participation">actively contribute</a> to the work of the Task Force. If you are interested in participating in the Silver TF, please send e-mail to @@ and include a little bit about what you’re interested in and how you think that you may be able to contribute to the Task Force. Then follow the <a href="../../participation">participation procedures of the Accessibility Guidelines Working Group</a>, and once you have joined, ask <a href="mailto:cooper@w3.org">Michael Cooper</a> to add you to the task force.</p>
-		<p><a href="https://www.w3.org/groups/tf/wcag2ict/participants">Current participants in the WCAG2ICT TF</a>.</p>
+		<p><a href="https://www.w3.org/groups/tf/wcag2ict/participants">WCAG2ICT Task Force - Participants</a> lists current and past participants.</p>
 		
 	</section> 
 	
@@ -73,7 +92,7 @@
 		</section>
 		<section id="work-statement">
 			<h3>Work Statement</h3>
-			<p>The <a href="work-statement">WCAG2ICT Task Force Work Statement</a> defines the initial objective, scope, approach, and participation of the Task Force.</p>
+			<p>The <a href="work-statement">Draft WCAG2ICT Task Force Work Statement</a> defines the initial objective, scope, approach, and participation of the Task Force. The draft work statement is subject to the review and approval of the Accessibility Guidelines WG.</p>
 		</section>
 	</section>
 	
