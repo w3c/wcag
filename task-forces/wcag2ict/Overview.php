8793deca7c4abe6fbd326b0c4050ced9a389760f
maryjom <maryjom@us.ibm.com>
2022-07-13 10:51:10 -0400

    Update Overview.php
    WCAG2ICT page: Fix a few minor issues that I thought I fixed before (e.g. remove wiki link), small editorials, add me as a chair, add email for TF interests.

--- task-forces/wcag2ict/Overview.php
+++ task-forces/wcag2ict/Overview.php
@@ -8,15 +8,15 @@
 <body>
 	<?php include "../../_header.phi"; ?>
 	<h1>WCAG2ICT Task Force</h1>
-	<p>The objective of theWCAG2ICT Task Force is to develop documentation describing how WCAG 2.1 and 2.2 and its principles, guidelines, and success criteria could apply to non-Web Information and Communications Technologies (ICT). </p>
+	<p>The objective of the WCAG2ICT Task Force is to develop documentation describing how WCAG 2.1 and 2.2 and its principles, guidelines, and success criteria could apply to non-Web Information and Communications Technologies (ICT). </p>
 	<section id="communication">
 		<h2>Meetings and Communication</h2>
 		<p>The WCAG2ICT TF conducts its work using a variety of synchronous and asynchronous tools. Most work is done via email and issues as shown below, supplemented by occasional teleconferences.</p>
 		<ul>
 			<li><a href="#email">Email lists</a>;</li>
-			<li><a href="wiki/">wiki</a>;</li>
+
 			<li><a href="https://github.com/w3c/wcag2ict/">wcag2ict GitHub repository</a> and <a href="https://w3c.github.io/wcag2ict/">GitHub draft documents</a>;</li>
-			<li>IRC discussion on the <a href="irc://irc.w3.org/wcag2ict">#wcag2ict</a> IRCchannel, used largely for minute-taking;</li>
+			<li>IRC discussion on the <a href="irc://irc.w3.org/wcag2ict">#wcag2ict</a> IRC channel, used largely for minute-taking;</li>
 			<li><a href="/2002/09/wbs/55145/">Web-Based Surveys (WBS)</a>;</li>
 			<li>Teleconferences of the task force (also see <a href="minutes">meeting minutes</a>);</li>
 			<li>Face to face meetings;</li>
@@ -54,7 +54,7 @@
 	
 	<section id="contribute">
 		<h2>How to Comment, Contribute, and Participate</h2>
-		<p>To join the WCAG2ICT TF, individuals must be participants of the Accessibility Guidelines Working Group. Participants are expected to <a href="work-statement#participation">actively contribute</a> to the work of the Task Force. If you are interested in participating in the Silver TF, please send e-mail to @@ and include a little bit about what you’re interested in and how you think that you may be able to contribute to the Task Force. Then follow the <a href="../../participation">participation procedures of the Accessibility Guidelines Working Group</a>, and once you have joined ask <a href="mailto:cooper@w3.org">Michael Cooper</a> to add you to the task force.</p>
+		<p>To join the WCAG2ICT TF, individuals must be participants of the Accessibility Guidelines Working Group. Participants are expected to <a href="work-statement#participation">actively contribute</a> to the work of the Task Force. If you are interested in participating in the WCAG2ICT TF, email the task force leadership and working group chairs at <a href="mailto:group-ag-chairs@w3.org">group-ag-chairs@w3.org</a> and include a little bit about what you’re interested in and how you think that you may be able to contribute to the Task Force. Then follow the <a href="../../participation">participation procedures of the Accessibility Guidelines Working Group</a>, and once you have joined ask <a href="mailto:cooper@w3.org">Michael Cooper</a> to add you to the task force.</p>
 		<p><a href="https://www.w3.org/groups/tf/wcag2ict/participants">Current participants in the WCAG2ICT TF</a>.</p>
 		
 	</section> 
@@ -65,7 +65,7 @@
 		<section id="facilitator">
 			<h3>Facilitator and Contacts</h3>
 			<ul>
-				<li><strong>Facilitators:</strong> TBD</li>
+				<li><strong>Facilitators:</strong> Mary Jo Mueller (IBM) and TBD</li>
 				<li><strong>Technical Staff Contact:</strong> <a href="https://www.w3.org/People/Brewer/">Judy Brewer</a></li>
 				<li><strong>Adminstrative Staff Contact:</strong> <a href="http://www.w3.org/People/cooper/">Michael Cooper</a></li>
 			</ul>
