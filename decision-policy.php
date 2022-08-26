121d2ab5e492f92f81353f9bc4ceef47d2b1d715
Michael Cooper <cooper@w3.org>
2022-08-26 09:39:35 -0400

    updates related to wcag 3 charter

--- decision-policy.php
+++ decision-policy.php
@@ -8,39 +8,70 @@
 	<body>
 		<?php include "_header.phi"; ?>
 		<h1>AG WG Decision Policy</h1>
-		<p>This document explains the decision process of the <a href="./">AG Working Group</a>. It was <a href="http://www.w3.org/2015/09/15-wai-wcag-minutes#item02">accepted by the Working Group 15 September 2015</a>. It was updated editorially 27 January 2017 to change the name of the WG to "Accessibility Guidelines Working Group" and to add links to referenced resources. A substantive update was <a href="https://lists.w3.org/Archives/Public/w3c-wai-gl/2018OctDec/0095.html">approved 20 November 2018</a>.</p>
-		<p>The Working Group follows the <a href="http://www.w3.org/Consortium/Process/#Consensus">W3C Process Document</a> regarding decisions. This document provides additional detail about the consensus decision-making process. </p>
-		<p>The Working Group strives to reach consensus via unanimous agreement. However, at times unanimity is not possible, and for the sake of continuing to work on important topics the group must arrive at a consensus decision and move forward. In the course of establishing consensus it is critical that all participants have the opportunity to express their views for consideration so that all relevant information can be used in arriving at the conclusion. Consensus indicates that a substantial number of individuals in the group support a proposal, and within the AG Working Group consensus can be achieved through this process. </p>
-		<p><a href="https://www.w3.org/2018/Process-20180201/#Consensus">Consensus</a> is not a vote. The exact number of working group participants supporting a Call for Consensus compared to objections is not the only factor in the decision. Although significant support from the active membership is always desirable, consensus means working through objections until they are resolved either through amending the decision or in rare cases overriding the objection as laid out in <a href="https://www.w3.org/2018/Process-20180201/#managing-dissent">Managing Dissent</a>. In order to work through objections they must have a clear rationale based on the technical merit or with reference to the agreed scope of the work. Moving on usually means a careful approach is taken. For example, <em>not</em> adding something to the documentation.</p>
-		<p>Additions to normative text such as new success criteria should have a pre-defined scope. For example, the <a href="https://www.w3.org/WAI/GL/wiki/WCAG_2.1_Success_Criteria">requirements for WCAG 2.1 success criteria</a>. Where new normative text does not reach consensus the reasons should be recorded, depending on the origin of the text. For example, if the new text originated in a github issue or pull request, it could be labeled with "Unsupported addition".</p>
-		<p>During discussion on a topic, participants are welcome to raise objections freely to help ensure that all available information can be considered and contribute to the best possible decision. However, when the chairs issue a Call for Consensus, objections should not be raised unless the individual strongly believes the decision is the wrong one in spite of discussion, and the individual cannot "live with" the decision. Compromise on points that the individual considers suboptimal but can "live with" is an essential part of group decisions that must meet various requirements. </p>
-		<ol>
-			<li> Discussion on a topic proceeds until the chairs believe that all points of view have been expressed and the group has considered the variety of information presented. Depending on the topic, this discussion may take a couple of days or a couple of weeks, or more.
-				<ol>
-					<li> Discussion can take place in any <a href="./communication">recognized channel of the Working Group</a> including email on the AG mailing list, comment threads for GitHub issues or pull requests, or on Working Group calls. </li>
-				</ol>
-			</li>
-			<li> When the chairs believe that the group is ready to come to a decision they announce a Call for Consensus by email to the Working Group's mailing list. The Call must remain open for a minimum of two working days.
-				<ol>
-					<li> The Call is open to responses from all group members. </li>
-					<li>The Call will be for a single topic, will clearly indicate that it is a Call for Consensus, and will contain pointers to the relevant discussion. This may include links to GitHub issues or pull requests, AG surveys, email threads, or meeting minutes.</li>
-					<li> A resolution recorded in a WG teleconference may precede a Call for Consensus, but it may not replace the official Call for Consensus. </li>
-					<li> Issues that are regarded as editorial by the Chairs do not require a Working Group decision in order for the Chairs to address, and thus do not require a Call for Consensus. If there is disagreement by participants on whether something is editorial this can be brought to the attention of the chairs either privately or in the context of the wider group. </li>
-				</ol>
-			</li>
-			<li>Evaluating the Call for Consensus.
-				<ol>
-					<li>If no objections are received by the deadline, the draft decision becomes a formal decision of the Working Group. </li>
-					<li>If objections are received but the chairs believe the objections have already been considered as far as is possible and reasonable, and the reviewers providing the objections can "live with” the decision, the draft decision becomes a formal decision of the Working Group. </li>
-					<li>If objections are received that the chairs believe present substantive new information or if the chairs believe there is not a clear consensus in the Working Group, they will reopen the discussion, as detailed in <a href="http://www.w3.org/Consortium/Process/#WGChairReopen">section 3.3.4 of the Process Document (Reopening a Decision When Presented With New Information)</a>.</li>
-					<li>If working group member(s) continue to disagree and the chairs do not believe it presents substantive new information, or it does not meet the criteria established for adding new normative content, the chairs may decide the draft decision becomes a formal decision of the Working Group despite the objection.</li>
-				</ol>
-			</li>
-			<li>The Working Group chairs record the Formal Decisions on the <a href="./wiki/Decisions">AG Decisions page</a> on the wiki. The change to the WCAG documents is incorporated into the editors draft and recorded in the appropriate change log.</li>
-			<li>In the rare situation where a <a href="https://www.w3.org/2018/Process-20180201/#FormalObjection">formal objection</a> is raised all the Chairs and staff contact(s) will come to an agreement about the issue and may record a decision with objections per <a href="http://www.w3.org/Consortium/Process/#WGArchiveMinorityViews">section 3.3.2 of the Process Document (Recording and Reporting Formal Objections)</a> and work to address the issue as described in <a href="http://www.w3.org/Consortium/Process/#formal-address">section 3.3.3 of the Process Document (Formally Addressing an Issue)</a>.</li>
-
-		</ol>
-		<p>Other aspects of the Working Group decision policy are defined in the Process Document, including <a href="http://www.w3.org/Consortium/Process/#Votes">Votes (section 3.4)</a> and <a href="http://www.w3.org/Consortium/Process/#WGAppeals">Appeal of a Chair’s Decision (section 3.5)</a>. If a participant believes the Chairs have not exercised sound judgment in following this policy, they should express their concern first to one of the Working Group Chairs, escalating if needed to the AG staff contact, and escalating if needed to the W3C Web Accessibility Initiative Lead. </p>
+		<p>This document explains the decision process of the <a href="./">AG Working Group</a>. It was <a href="http://www.w3.org/2015/09/15-wai-wcag-minutes#item02">accepted by the Working Group 15 September 2015</a>. It was updated editorially 27 January 2017 to change the name of the WG to "Accessibility Guidelines Working Group" and to add links to referenced resources. <a href="https://lists.w3.org/Archives/Public/w3c-wai-gl/2018OctDec/0095.html">A substantive update was approved 20 November 2018</a>. <a href="https://lists.w3.org/Archives/Public/w3c-wai-gl/2022JulSep/0153.html">Substantive changes to support WCAG 3 work were approved 5 August 2022</a>.</p>
+			<p>The Working Group follows the <a href="http://www.w3.org/Consortium/Process/#Consensus">W3C Process Document</a> regarding decisions. This document provides additional detail about the consensus decision-making process. The Working Group updates this policy as needed, using the procedures of this policy to ratify the update.</p>
+			<p><strong>For mature (ready to go to wide review) content:</strong> The Working Group strives for unanimous agreement. However, at times unanimity is not possible, and for the sake of continuing to work on important topics the group must arrive at a consensus decision and move forward. In the course of establishing consensus it is critical that all participants have the opportunity to express their views for consideration so that all relevant information can be used in arriving at the conclusion.</p>
+			<p><a href="https://www.w3.org/Consortium/Process/#Consensus">Consensus</a> is not a vote. Consensus indicates that a substantial number of individuals in the group support a proposal. The exact number of working group participants supporting a Call for Consensus and any objections are not the only factors in the decision. Although significant support from the active membership is always desirable, consensus means working through objections until they are resolved either through amending the decision or in rare cases overriding the objection as laid out in <a href="https://www.w3.org/Consortium/Process/#managing-dissent">Managing Dissent</a>. Objections must have a clear rationale based on the technical merit or with reference to the agreed scope of the work. Moving on usually means a careful approach is taken. For example, <i>not</i> adding something to the documentation.</p>
+			<p><strong>For content that is not yet ready to go to wide review as “mature”,</strong> the Working Group strives to reach broad consensus only. Instead of objections, outstanding questions and concerns are captured as editors notes. These questions and concerns must be addressed before the content is sent for wide public review, though smaller, targeted releases may occur. The group moves forward once all questions and concerns are captured, most members agree, and the Chairs agree that the content is worth pursuing.</p>
+			<p>During discussion on a topic, participants are welcome to raise objections freely to help ensure that all
+		available information can be considered and contribute to the best possible decision. However, when the Chairs issue a Call for Consensus, objections should not be raised unless the individual has strong technical reasons that the decision is the wrong one in spite of discussion, and the individual cannot accept the decision. Compromise on points that the individual considers suboptimal but can accept is an essential part of group decisions that must meet various requirements.</p>
+			<ol>
+				<li>
+					<p>Discussion on a topic proceeds until the Chairs believe that all points of view have been expressed and the group has considered the variety of information presented. Depending on the topic, this discussion may take a couple of days or a couple of weeks, or more.</p>
+					<ol>
+						<li>
+							<p>Discussion can take place in any <a href="https://www.w3.org/WAI/GL/communication">recognized channel of the Working Group</a> including email on the AG mailing list, comment threads for GitHub issues or pull requests, or on Working Group calls.</p>
+						</li>
+					</ol>
+				</li>
+				<li>
+					<p>When the Chairs believe that the group is ready to come to a decision they will propose a resolution for a vote using the Call for Consensus process. Optionally, a “final call” email can be used to prepare the group for the upcoming CFC and capture any outstanding comments. After the CFC, the decision is recorded as a resolution.</p>
+					<ol>
+						<li>The Call must remain open for a minimum of 4 working days. If a CFC is re-started, it can be closed after 2 additional business days, or the original 4 working days (whichever is longer).</li>
+					</ol>
+				</li>
+				<li>
+					<p>If the decision affects mature normative text, Chairs will then announce a Call for Consensus by email to the Working Group's mailing list. The Call must remain open for a minimum of two working days.</p>
+					<ol>
+						<li>
+							<p>The Call is open to responses from all group members.</p>
+						</li>
+						<li>
+							<p>The Call will be for a single topic, will clearly indicate that it is a Call for Consensus, and will contain pointers to the relevant discussion. This may include links to GitHub issues or pull requests, AG surveys, email threads, or meeting minutes.</p>
+						</li>
+						<li>
+							<p>A resolution recorded in a WG teleconference may precede a Call for Consensus, but it does not replace the official Call for Consensus.</p>
+						</li>
+						<li>
+							<p>Issues that are regarded as editorial by the Chairs do not require a Working Group decision in order for the Chairs to address, and thus do not require a Call for Consensus. If there is disagreement by participants on whether something is editorial this can be brought to the attention of the Chairs either privately or in the context of the wider group.</p>
+						</li>
+					</ol>
+				</li>
+				<li>
+					<p>Evaluating the Call for Consensus.</p>
+					<ol>
+						<li>
+							<p>If no objections are received by the deadline, the draft decision becomes a formal decision of the Working Group.</p>
+						</li>
+						<li>
+							<p>If objections are received but the Chairs believe the objections have already been considered as far as is possible and reasonable, and the reviewers providing the objections can accept the decision, the draft decision becomes a formal decision of the Working Group.</p>
+						</li>
+						<li>
+							<p>If objections are received that the Chairs believe present substantive new information or if the Chairs believe there is not a clear consensus in the Working Group, they will reopen the discussion, as detailed in <a href="http://www.w3.org/Consortium/Process/#WGChairReopen">section 3.3.4 of the Process Document (Reopening a Decision When Presented With New Information)</a>.</p>
+						</li>
+						<li>
+							<p>If working group member(s) continue to disagree and the Chairs do not believe it presents substantive new information, or it does not meet the criteria established for adding new normative content, the Chairs may decide the draft decision becomes a formal decision of the Working Group despite the objection.</p>
+						</li>
+					</ol>
+				</li>
+				<li>
+					<p>The Working Group Chairs record the Formal Decisions on the <a href="https://www.w3.org/WAI/GL/wiki/Decisions">AG Decisions page</a> on the wiki, along with references to any objections. The change to the WCAG documents is incorporated into the editors draft and recorded in the appropriate change log.</p>
+				</li>
+				<li>
+					<p>If an objection is considered serious enough to warrant additional review before a deliverable advances to the next formal stage, a formal objection may be filed. A formal objection should be raised as a public response to the Call for Consensus to advance to the next stage, using the words “formal objection” for clarity. Chairs and staff will work to address the issue <a href="https://www.w3.org/2021/Process-20211102/#WGArchiveMinorityViews">Recording and Reporting Formal Objections</a>.</p>
+				</li>
+			</ol>
+			<p>Other aspects of the Working Group decision policy are defined in the Process Document, including in <a href="https://www.w3.org/2021/Process-20211102/#formal-address">Formally Addressing an Issue</a>, <a href="https://www.w3.org/2021/Process-20211102/#Votes">Votes</a> and <a href="https://www.w3.org/2021/Process-20211102/#WGAppeals">Appeal of a Chair’s Decision</a>. If a participant believes the Chairs have not exercised sound judgment in following this policy, they should express their concern first to one of the Working Group Chairs, escalating if needed to the AG staff contact, WAI Domain Lead, or Project Lead, and escalating if needed to their AC representative.</p>
 		<?php include "_footer.phi"; ?>
 	</body>
 </html>
