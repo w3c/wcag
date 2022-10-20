863c3634d7e62ed8e99f360796b993dac6a3799b
rachaelbradley <rachaelbradley@users.noreply.github.com>
2022-10-20 13:26:09 -0400

    Update decision-policy.php
    Changes per leadership email conversation

--- decision-policy.php
+++ decision-policy.php
@@ -57,10 +57,10 @@
 							<p>If objections are received but the Chairs believe the objections have already been considered as far as is possible and reasonable, and the reviewers providing the objections can accept the decision, the draft decision becomes a formal decision of the Working Group.</p>
 						</li>
 						<li>
-							<p>If objections are received and the reviewers providing the objection cannot accept the decision, the Chairs will provide the opportunity to propose an alternative, and have the Working Group review the alternative.</p>
+							<p>When a decision is made with objections, the Chairs will provide the opportunity to propose an alternative based on the consensus decision, and have the Working Group review the alternative.</p>
 						</li>
 						<li>
-							<p>If objections are received that the Chairs believe present substantive new information or if the Chairs believe there is not a clear consensus in the Working Group, they will reopen the discussion, as detailed in <a href="http://www.w3.org/Consortium/Process/#WGChairReopen">section 3.3.4 of the Process Document (Reopening a Decision When Presented With New Information)</a>.</p>
+							<p>If objections are received that the Chairs believe present substantive new information or if the Chairs believe there is not a clear consensus in the Working Group, they will reopen the discussion, as detailed in <a href="http://www.w3.org/Consortium/Process/#WGChairReopen">section 3.3.4 of the Process Document (Reopening a Decision When Presented With New Information)</a>. A new alternative version that might get consensus would be considered substantive new information.</p>
 						</li>
 						<li>
 							<p>If working group member(s) continue to disagree and the Chairs do not believe it presents substantive new information, or it does not meet the criteria established for adding new normative content, the Chairs may decide the draft decision becomes a formal decision of the Working Group despite the objection.</p>
