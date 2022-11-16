265ce93e42981d273c820f6c3cfae836f4779e46
Michael Cooper <cooper@w3.org>
2022-11-16 10:44:28 -0500

    Revert "Replace 4.1.1 with ednote"
    This reverts commit 2b6caecf0ce46dad19306d25b94a10891c51d936.

--- script/wcag.js
+++ script/wcag.js
@@ -81,24 +81,10 @@ function termTitles() {
 	});	
 }
 
-// clear removed sections after respec has run to preserve generated numbering
-function clearRemoved() {
-  const removeIds = ["parsing"];
-  removeIds.forEach(function(currentValue) {
-    var selector = ".tocline > a[href=\"#" + currentValue + "\"]";
-    var tocEl = document.querySelector(selector);
-    tocEl.remove();
-    
-    var section = document.querySelector("#" + currentValue);
-    section.remove();
-  });
-}
-
 // scripts after Respec has run
 function postRespec() {
 	addTextSemantics();
 	swapInDefinitions();
 	termTitles();
 	linkUnderstanding();
-	clearRemoved();
 }
