561c4b29b1047dd53b092ea735329668ecb4dacc
fstrr <francis.storr@intel.com>
2023-01-30 10:46:00 -0800

    Update scr32
    - code blocks in `code` elms
    - updated examples to add autocomplete attributes
    - updated working example to include `lang` attribute, up-to-date `doctype`, `charset`, and add a viewport tag.
    - updated zip file accordingly.

--- working-examples/script-form-validation-2/index.php
+++ working-examples/script-form-validation-2/index.php
@@ -68,13 +68,14 @@
 			$strError = "<h2>$iErrorCount Errors in Submission</h2>\n" . $strError;
 	}
 ?>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
-<html>
+<!DOCTYPE HTML>
+<html lang="en">
 <head>
+	<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">	
 	<title>Form Validation</title>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<link href="css/validate.css" rel="stylesheet" type="text/css">
-	<script type="text/javascript" src="scripts/validate.js"></script>
+	<link href="css/validate.css" rel="stylesheet">
+	<script src="scripts/validate.js"></script>
 </head>
 <body>
 
@@ -106,22 +107,22 @@ Please enter your details below.
 <div class="validationerrors"></div>
 <fieldset>
 <legend>Personal Details</legend>
-<p>
+<div>
 <label for="forename">Please enter your forename</label>
-<input type="text" size="20" name="forename" id="forename" class="string" value="<?php echo htmlspecialchars($strForename); ?>">
-</p>
-<p>
+<input autocomplete="given-name" type="text" size="20" name="forename" id="forename" class="string" value="<?php echo htmlspecialchars($strForename); ?>">
+</div>
+<div>
 <label for="age">Please enter your age</label> 
 <input type="text" size="20" name="age" id="age" class="number" value="<?php echo htmlspecialchars($strAge); ?>">
-</p>
-<p>
+</div>
+<div>
 <label for="email">Please enter your email address</label> 
-<input type="text" size="20" name="email" id="email" class="email" value="<?php echo htmlspecialchars($strEmail); ?>">
-</p>
+<input autocomplete="email" type="text" size="20" name="email" id="email" class="email" value="<?php echo htmlspecialchars($strEmail); ?>">
+</div>
 </fieldset>
-<p>
+<div>
 <input type="submit" name="signup" value="Sign up">
-</p>
+</div>
 </form>
 <?php
 	}
@@ -136,26 +137,26 @@ Please enter your details below.
 <div class="validationerrors"></div>
 <fieldset>
 <legend>Second Form Details</legend>
-<p>
+<div>
 <label for="suggestion">Enter a suggestion</label>
 <input type="text" size="20" name="suggestion" id="suggestion" class="string" value="<?php echo htmlspecialchars($strSuggestion); ?>">
-</p>
-<p>
+</div>
+<div>
 <label for="optemail">Please enter your email address (optional)</label>
-<input type="text" size="20" name="optemail" id="optemail" class="optional email" value="<?php echo htmlspecialchars($strOptional); ?>">
-</p>
-<p>
+<input autocomplete="email" type="text" size="20" name="optemail" id="optemail" class="optional email" value="<?php echo htmlspecialchars($strOptional); ?>">
+</div>
+<div>
 <label for="rating">Please rate this suggestion</label> 
 <input type="text" size="20" name="rating" id="rating" class="number" value="<?php echo htmlspecialchars($strRating); ?>">
-</p>
-<p>
+</div>
+<div>
 <label for="jibberish">Enter some jibberish (optional)</label>
 <input type="text" size="20" name="jibberish" id="jibberish" value="<?php echo htmlspecialchars($strJibberish); ?>">
-</p>
+</div>
 </fieldset>
-<p>
+<div>
 <input type="submit" name="submit" value="Add Suggestion">
-</p>
+</div>
 </form>
 <?php } ?>
 </body>
