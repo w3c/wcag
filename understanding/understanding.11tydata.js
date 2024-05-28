31b485356a57abcc4ce29d68ae85413871b6f3f7
Kenneth G. Franqueiro <kfranqueiro@users.noreply.github.com>
2024-05-28 13:08:13 -0400

    Understanding: Add eleventyComputed for flattening URLs

--- understanding/understanding.11tydata.js
+++ understanding/understanding.11tydata.js
@@ -3,5 +3,10 @@ export default function (data) {
         headerLabel: "Understanding Docs", // i.e. documentset.name
         headerUrl: data.understandingUrl,
         isUnderstanding: true,
+        eleventyComputed: {
+            // Flatten pages into top-level directory, out of version subdirectories
+            permalink: (data) =>
+                `${data.page.filePathStem.replace(/\/2\d/, "")}/index.html`,
+        }
     };
 }
