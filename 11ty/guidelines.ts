a68204ccfd8c83dc02053eb37bc419d07fd5e889
Kenneth G. Franqueiro <kfranqueiro@users.noreply.github.com>
2024-07-10 11:53:57 -0400

    WIP Add support for generating WCAG 2.1 from main branch

--- 11ty/guidelines.ts
+++ 11ty/guidelines.ts
@@ -34,39 +34,20 @@ export const actRules = (
 )["act-rules"];
 
 /**
- * Returns an object with keys for each existing WCAG 2 version,
- * each mapping to an array of basenames of HTML files under understanding/<version>
- * (Functionally equivalent to "guidelines-versions" target in build.xml)
+ * Returns an object mapping each existing WCAG 2 SC slug to the earliest WCAG version it applies to.
+ * (Functionally equivalent to "guidelines-versions" target in build.xml, structurally inverted)
  */
-export async function getGuidelinesVersions() {
+async function getSuccessCriteriaVersions() {
   const paths = await glob("*/*.html", { cwd: "understanding" });
-  const versions: Record<WcagVersion, string[]> = { "20": [], "21": [], "22": [] };
+  const map: Record<string, WcagVersion> = {};
 
   for (const path of paths) {
-    const [version, filename] = path.split("/");
-    assertIsWcagVersion(version);
-    versions[version].push(basename(filename, ".html"));
+    const [fileVersion, filename] = path.split("/");
+    assertIsWcagVersion(fileVersion);
+    map[basename(filename, ".html")] = fileVersion;
   }
 
-  for (const version of Object.keys(versions)) {
-    assertIsWcagVersion(version);
-    versions[version].sort();
-  }
-  return versions;
-}
-
-/**
- * Like getGuidelinesVersions, but mapping each basename to the version it appears in
- */
-export async function getInvertedGuidelinesVersions() {
-  const versions = await getGuidelinesVersions();
-  const invertedVersions: Record<string, string> = {};
-  for (const [version, basenames] of Object.entries(versions)) {
-    for (const basename of basenames) {
-      invertedVersions[basename] = version;
-    }
-  }
-  return invertedVersions;
+  return map;
 }
 
 export interface DocNode {
@@ -79,7 +60,7 @@ export interface DocNode {
 export interface Principle extends DocNode {
   content: string;
   num: `${number}`; // typed as string for consistency with guidelines/SC
-  version: "WCAG20";
+  version: "20";
   guidelines: Guideline[];
   type: "Principle";
 }
@@ -87,7 +68,7 @@ export interface Principle extends DocNode {
 export interface Guideline extends DocNode {
   content: string;
   num: `${Principle["num"]}.${number}`;
-  version: `WCAG${"20" | "21"}`;
+  version: "20" | "21";
   successCriteria: SuccessCriterion[];
   type: "Guideline";
 }
@@ -97,13 +78,20 @@ export interface SuccessCriterion extends DocNode {
   num: `${Guideline["num"]}.${number}`;
   /** Level may be empty for obsolete criteria */
   level: "A" | "AA" | "AAA" | "";
-  version: `WCAG${WcagVersion}`;
+  version: WcagVersion;
   type: "SC";
 }
 
 export function isSuccessCriterion(criterion: any): criterion is SuccessCriterion {
   return !!(criterion?.type === "SC" && "level" in criterion);
 }
+
+/** Defines version-dependent partial overrides of SC metadata for older versions. */
+export const scOverrides: Record<string, (version: WcagVersion) => Partial<SuccessCriterion>> = {
+  parsing: (version) => (version < "22" ? { level: "A", name: "Parsing" } : {}),
+  "target-size-enhanced": (version) =>
+    version < "22" ? { id: "target-size", name: "Target Size" } : {},
+};
 
 /**
  * Returns HTML content used for Understanding guideline/SC boxes.
@@ -121,7 +109,7 @@ const getContentHtml = ($el: Cheerio<Element>) => {
  * comparable to the principles section of wcag.xml from the guidelines-xml Ant task.
  */
 export async function getPrinciples() {
-  const versions = await getInvertedGuidelinesVersions();
+  const versions = await getSuccessCriteriaVersions();
   const $ = await flattenDomFromFile("guidelines/index.html");
 
   const principles: Principle[] = [];
@@ -130,17 +118,15 @@ export async function getPrinciples() {
     $(".guideline", el).each((j, guidelineEl) => {
       const successCriteria: SuccessCriterion[] = [];
       $(".sc", guidelineEl).each((k, scEl) => {
-        const resolvedVersion = versions[scEl.attribs.id];
-        assertIsWcagVersion(resolvedVersion);
-
+        const scId = scEl.attribs.id;
         successCriteria.push({
           content: getContentHtml($(scEl)),
-          id: scEl.attribs.id,
+          id: scId,
           name: $("h4", scEl).text().trim(),
           num: `${i + 1}.${j + 1}.${k + 1}`,
           level: $("p.conformance-level", scEl).text().trim() as SuccessCriterion["level"],
           type: "SC",
-          version: `WCAG${resolvedVersion}`,
+          version: versions[scId],
         });
       });
 
@@ -150,7 +136,7 @@ export async function getPrinciples() {
         name: $("h3", guidelineEl).text().trim(),
         num: `${i + 1}.${j + 1}`,
         type: "Guideline",
-        version: guidelineEl.attribs.id === "input-modalities" ? "WCAG21" : "WCAG20",
+        version: guidelineEl.attribs.id === "input-modalities" ? "21" : "20",
         successCriteria,
       });
     });
@@ -161,12 +147,36 @@ export async function getPrinciples() {
       name: $("h2", el).text().trim(),
       num: `${i + 1}`,
       type: "Principle",
-      version: "WCAG20",
+      version: "20",
       guidelines,
     });
   });
 
   return principles;
+}
+
+/**
+ * Given an unfiltered principles array and a target WCAG version,
+ * applies version-specific patches and filters out irrelevant Guidelines/SC.
+ * @param allPrinciples Unfiltered principles array from getPrinciples
+ * @param version WCAG version to filter and patch principles data for
+ * @returns
+ */
+export function getPrinciplesForVersion(allPrinciples: Principle[], version: WcagVersion) {
+  return allPrinciples.map((principle) => ({
+    ...principle,
+    guidelines: principle.guidelines
+      .filter((guideline) => guideline.version <= version)
+      .map((guideline) => ({
+        ...guideline,
+        successCriteria: guideline.successCriteria
+          .filter((sc) => sc.version <= version)
+          .map((sc) => ({
+            ...sc,
+            ...(sc.id in scOverrides && scOverrides[sc.id](version)),
+          })),
+      })),
+  }));
 }
 
 /**
