4402e8fffa79b342b093af9098428f1400aabd4c
Kenneth G. Franqueiro <kfranqueiro@users.noreply.github.com>
2024-10-07 12:48:07 -0400

    Revert "2.1 support: Pin ACT mappings to requested version"
    This reverts commit 89e759363bd0c26c26a0754f3a3c472564a4d9bd.
    
    After discussion with Daniel who conferred with ACT planning team,
    the version of act-mapping.json currently on the WCAG-2.1 branch
    is indeed very out of date and not something to preserve.

--- eleventy.config.ts
+++ eleventy.config.ts
@@ -6,9 +6,8 @@ import { copyFile } from "fs/promises";
 import { CustomLiquid } from "11ty/CustomLiquid";
 import { resolveDecimalVersion } from "11ty/common";
 import {
+  actRules,
   assertIsWcagVersion,
-  getActRules,
-  getActRulesForVersion,
   getFlatGuidelines,
   getPrinciples,
   getPrinciplesForVersion,
@@ -47,10 +46,6 @@ const isTechniqueObsolete = (technique: Technique | undefined) =>
  */
 const isGuidelineObsolete = (guideline: Principle | Guideline | SuccessCriterion | undefined) =>
   guideline?.type === "SC" && guideline.level === "";
-
-const actRules = process.env.WCAG_VERSION
-  ? await getActRulesForVersion(version)
-  : await getActRules();
 
 /** Tree of Principles/Guidelines/SC across all versions (including later than selected) */
 const allPrinciples = await getPrinciples();
