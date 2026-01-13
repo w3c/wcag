4e35c224131553958fdb8380b2ad17c829713571
Adam Page <adam@adampage.net>
2026-01-13 13:34:09 -0800

    re-number F999 to F114

--- understanding/understanding.11tydata.js
+++ understanding/understanding.11tydata.js
@@ -293,7 +293,7 @@ export default function (data) {
           },
         ],
         advisory: ["C22", "G162", "ARIA1", "ARIA2", "G141"],
-        failure: ["F2", "F33", "F34", "F42", "F43", "F46", "F48", "F90", "F91", "F92", "F111", "F999"],
+        failure: ["F2", "F33", "F34", "F42", "F43", "F46", "F48", "F90", "F91", "F92", "F111", "F114"],
       },
 
       "meaningful-sequence": {
@@ -314,7 +314,7 @@ export default function (data) {
           "C27",
           "PDF3",
         ],
-        failure: ["F34", "F33", "F32", "F49", "F1", "F999"],
+        failure: ["F34", "F33", "F32", "F49", "F1", "F114"],
       },
 
       "sensory-characteristics": {
