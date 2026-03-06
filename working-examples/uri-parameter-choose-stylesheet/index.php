6d2d113293abc5324a3783ac48023ca7e6f8c2ef
Francis Storr <francis.storr@intel.com>
2026-03-06 10:53:47 -0800

    remove extra attrs from html element

--- working-examples/uri-parameter-choose-stylesheet/index.php
+++ working-examples/uri-parameter-choose-stylesheet/index.php
@@ -11,10 +11,11 @@ else
 	}
 ?>
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html lang="en">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <title>Using PHP $_GET to apply a different external CSS file</title>
    
 
